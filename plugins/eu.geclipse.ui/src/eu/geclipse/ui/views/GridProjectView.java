c89b0cd2c76d998d099b45423edc3c4395e1d57b
Mathias Stümpert <mathias.stuempert@iwr.fzk.de>
2007-04-13 14:59:50 +0000

    cleaned up imports

--- plugins/eu.geclipse.ui/src/eu/geclipse/ui/views/GridProjectView.java
+++ plugins/eu.geclipse.ui/src/eu/geclipse/ui/views/GridProjectView.java
@@ -15,9 +15,6 @@
 
 package eu.geclipse.ui.views;
 
-import org.eclipse.core.resources.IResourceChangeEvent;
-import org.eclipse.core.resources.IResourceChangeListener;
-import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.jface.viewers.IBaseLabelProvider;
 import org.eclipse.jface.viewers.IContentProvider;
 import org.eclipse.jface.viewers.StructuredViewer;
