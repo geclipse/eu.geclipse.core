a1be9f9c24cdebb089906f9a06dce5d9462e2b2e
Pawel Wolniewicz <pwolniewi>
2007-04-18 13:23:16 +0000

    Reformat to avoid checkstyle errors

--- plugins/eu.geclipse.jsdl.adapters/src/eu/geclipse/jsdl/adapters/internal/JsdlAdaptersPlugin.java
+++ plugins/eu.geclipse.jsdl.adapters/src/eu/geclipse/jsdl/adapters/internal/JsdlAdaptersPlugin.java
@@ -8,43 +8,43 @@ import org.osgi.framework.BundleContext;
  */
 public class JsdlAdaptersPlugin extends Plugin {
 
-	// The plug-in ID
-	public static final String PLUGIN_ID = "eu.geclipse.jsdl.adapters";
+  // The plug-in ID
+  public static final String PLUGIN_ID = "eu.geclipse.jsdl.adapters";
+  // The shared instance
+  private static JsdlAdaptersPlugin plugin;
 
-	// The shared instance
-	private static JsdlAdaptersPlugin plugin;
-	
-	/**
-	 * The constructor
-	 */
-	public JsdlAdaptersPlugin() {
-		plugin = this;
-	}
+  /**
+   * The constructor
+   */
+  public JsdlAdaptersPlugin() {
+    plugin = this;
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.core.runtime.Plugins#start(org.osgi.framework.BundleContext)
-	 */
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-	}
+  /*
+   * (non-Javadoc)
+   * 
+   * @see org.eclipse.core.runtime.Plugins#start(org.osgi.framework.BundleContext)
+   */
+  public void start( BundleContext context ) throws Exception {
+    super.start( context );
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
-	 */
-	public void stop(BundleContext context) throws Exception {
-		plugin = null;
-		super.stop(context);
-	}
+  /*
+   * (non-Javadoc)
+   * 
+   * @see org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
+   */
+  public void stop( BundleContext context ) throws Exception {
+    plugin = null;
+    super.stop( context );
+  }
 
-	/**
-	 * Returns the shared instance
-	 *
-	 * @return the shared instance
-	 */
-	public static JsdlAdaptersPlugin getDefault() {
-		return plugin;
-	}
-
+  /**
+   * Returns the shared instance
+   * 
+   * @return the shared instance
+   */
+  public static JsdlAdaptersPlugin getDefault() {
+    return plugin;
+  }
 }
