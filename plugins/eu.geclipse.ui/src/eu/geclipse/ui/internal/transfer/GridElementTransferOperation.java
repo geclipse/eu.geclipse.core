572006a8a3122a0d6431e75b689c7ce8b6be08fa
Mathias Stümpert <mathias.stuempert@iwr.fzk.de>
2007-04-20 13:06:08 +0000

    fixed some bugs and implemented proper error handling

--- plugins/eu.geclipse.ui/src/eu/geclipse/ui/internal/transfer/GridElementTransferOperation.java
+++ plugins/eu.geclipse.ui/src/eu/geclipse/ui/internal/transfer/GridElementTransferOperation.java
@@ -9,11 +9,13 @@ import org.eclipse.core.filesystem.IFileStore;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.MultiStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.OperationCanceledException;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.SubProgressMonitor;
 import org.eclipse.core.runtime.jobs.Job;
+import eu.geclipse.core.model.GridModelException;
 import eu.geclipse.core.model.IGridConnectionElement;
 import eu.geclipse.core.model.IGridContainer;
 import eu.geclipse.core.model.IGridElement;
@@ -45,6 +47,7 @@ public class GridElementTransferOperation
     return this.globalTarget;
   }
   
+  @Override
   public IStatus run( final IProgressMonitor monitor ) {
     
     IProgressMonitor localMonitor
@@ -52,168 +55,362 @@ public class GridElementTransferOperation
       ? new NullProgressMonitor()
       : monitor;
       
-    String errors = ""; //$NON-NLS-1$
+    MultiStatus status = new MultiStatus( Activator.PLUGIN_ID, IStatus.OK, "Transfer operation status", null );
+      
     localMonitor.beginTask( "Transfering elements ... ", this.elements.length );
-    try {
+    
+    for ( IGridElement element : this.elements ) {
       
-      for ( IGridElement element : this.elements ) {
-        
-        localMonitor.subTask( element.getName() );
-        
-        IProgressMonitor subMonitor = new SubProgressMonitor( localMonitor, 1 );
-        try {
-          transferElement( element, this.globalTarget, subMonitor );
-        } catch ( CoreException cExc ) {
-          // TODO mathias proper exception handling
-          errors += cExc.getLocalizedMessage() + "; "; //$NON-NLS-1$
-        } finally {
-          subMonitor.done();
-        }
-        
-        if ( localMonitor.isCanceled() ) {
-          throw new OperationCanceledException();
-        }
-        
+      localMonitor.subTask( element.getName() );
+      
+      IProgressMonitor subMonitor = new SubProgressMonitor( localMonitor, 1 );
+      IStatus tempStatus = transferElement( element, this.globalTarget, subMonitor );
+      
+      if ( !tempStatus.isOK() ) {
+        status.merge( tempStatus );
       }
       
-    } finally {
-      localMonitor.done();
-      if ( errors.length() > 0 ) {
-        //throw new GridException( UIProblems.ELEMENT_TRANSFER_FAILED, errors );
+      if ( localMonitor.isCanceled() ) {
+        throw new OperationCanceledException();
       }
+      
     }
     
-    return Status.OK_STATUS;
+    return status;
     
   }
   
-  private void copy( final IFileStore from,
-                     final IFileStore to,
-                     final IProgressMonitor monitor )
-      throws CoreException {
+  private IStatus copy( final IFileStore from,
+                        final IFileStore to,
+                        final IProgressMonitor monitor ) {
     
+    IStatus status = Status.OK_STATUS;
     IFileInfo fromInfo = from.fetchInfo();
+    
     if ( fromInfo.isDirectory() ) {
-      copyDirectory( from, to, monitor );
+      status = copyDirectory( from, to, monitor );
     } else {
-      try {
-        copyFile( from, to, monitor );
-      } catch( IOException ioExc ) {
-        // TODO mathias proper exception handling
-        Activator.logException( ioExc );
-      }
+      status = copyFile( from, to, monitor );
     }
+    
+    return status;
     
   }
   
-  private void copyDirectory( final IFileStore from,
-                              final IFileStore to,
-                              final IProgressMonitor monitor )
-      throws CoreException {
+  private IStatus copyDirectory( final IFileStore from,
+                                 final IFileStore to,
+                                 final IProgressMonitor monitor ) {
     
+    // Prepare operation
+    IStatus status = Status.OK_STATUS;
     monitor.beginTask( "Copying " + from.getName(), 10 );
     
     try {
       
-      IFileStore[] children = from.childStores( EFS.NONE, new SubProgressMonitor( monitor, 1 ) );
-      IFileStore newTo = to.getChild( from.getName() );
-      IFileInfo newToInfo = newTo.fetchInfo();
-      boolean mkdir = !newToInfo.exists();
-      
-      SubProgressMonitor subMonitor = new SubProgressMonitor( monitor, 9 );
-      subMonitor.beginTask( "Copying " + from.getName(), mkdir ? children.length + 1 : children.length );
-      
-      if ( mkdir ) {
-        newTo.mkdir( EFS.NONE, new SubProgressMonitor( subMonitor, 1 ) );
+      IFileStore[] children = null;
+      try {
+        children = from.childStores( EFS.NONE, new SubProgressMonitor( monitor, 1 ) );
+      } catch( CoreException cExc ) {
+        status = new Status( IStatus.ERROR,
+                             Activator.PLUGIN_ID,
+                             IStatus.OK,
+                             "Error while getting children from " + from.getName(),
+                             cExc );
       }
       
-      for ( IFileStore child : children ) {
-        copy( child, newTo, new SubProgressMonitor( subMonitor, 1) );
-        if ( subMonitor.isCanceled() ) break;
+      if ( status.isOK() ) {
+        
+        IFileStore newTo = to.getChild( from.getName() );
+        IFileInfo newToInfo = newTo.fetchInfo();
+        boolean mkdir = !newToInfo.exists();
+      
+        SubProgressMonitor subMonitor = new SubProgressMonitor( monitor, 9 );
+        subMonitor.beginTask( "Copying " + from.getName(), mkdir ? children.length + 1 : children.length );
+      
+        if ( mkdir ) {
+          try {
+            newTo.mkdir( EFS.NONE, new SubProgressMonitor( subMonitor, 1 ) );
+          } catch( CoreException cExc ) {
+            status = new Status( IStatus.ERROR,
+                                 Activator.PLUGIN_ID,
+                                 IStatus.OK,
+                                 "Unable to create directory " + newTo.getName(),
+                                 cExc );
+          }
+        }
+      
+        if ( status.isOK() ) {
+        
+          MultiStatus mStatus
+            = new MultiStatus( Activator.PLUGIN_ID, IStatus.OK, "Copying members from " + from.getName(), null );
+          
+          for ( IFileStore child : children ) {
+            
+            IStatus tempStatus = copy( child, newTo, new SubProgressMonitor( subMonitor, 1) );
+            
+            if ( !tempStatus.isOK() ) {
+              mStatus.merge( tempStatus );
+            }
+            
+            if ( subMonitor.isCanceled() ) break;
+            
+          }
+          
+          if ( !mStatus.isOK() ) {
+            status = mStatus;
+          }
+        
+        }
+        
       }
       
     } finally {
       monitor.done();
     }
     
+    return status;
+    
   }
   
-  private void copyFile( final IFileStore from,
-                         final IFileStore to,
-                         final IProgressMonitor monitor )
-      throws CoreException, IOException {
+  private IStatus copyFile( final IFileStore from,
+                            final IFileStore to,
+                            final IProgressMonitor monitor ) {
     
+    // Prepare copy operation
     monitor.beginTask( "Copying " + from.getName(), 10 );
-    
     InputStream inStream = null;
     OutputStream outStream = null;
+    IStatus status = Status.OK_STATUS;
     
+    // Put in try-catch-clause to ensure that monitor.done() is called and streams are closed
     try {
     
+      // Fetch file infos
       IFileInfo fromInfo = from.fetchInfo();
       long length = fromInfo.getLength();
-      inStream = from.openInputStream( EFS.NONE, new SubProgressMonitor( monitor, 1 ) );
-      
       IFileStore toStore = to.getChild( from.getName() );
-      IFileInfo toInfo = to.fetchInfo();
-      if ( !toInfo.exists() ) {
-        
-        outStream = toStore.openOutputStream( EFS.NONE, new SubProgressMonitor( monitor, 1 ) );
+      IFileInfo toInfo = toStore.fetchInfo();
+            
+      // Test if target exists and omit operation in case it exists
+
+      if ( toInfo.exists() ) {
+        status = new Status( IStatus.ERROR,
+                             Activator.PLUGIN_ID,
+                             IStatus.OK,
+                             "Unable to transfer file " + toStore.getName() + " since destination file already exists in " + to.getName(),
+                             null );
+      }
+      
+      // Open input stream
+      if ( status.isOK() ) {
+        try {
+          inStream = from.openInputStream( EFS.NONE, new SubProgressMonitor( monitor, 1 ) );
+        } catch( CoreException cExc ) {
+          status = new Status( IStatus.ERROR,
+                               Activator.PLUGIN_ID,
+                               IStatus.OK,
+                               "Unable to open input stream for " + from.getName(),
+                               cExc );
+        }
+      }
+      
+      // Open output stream
+      if ( status.isOK() ) {
+        try {
+          outStream = toStore.openOutputStream( EFS.NONE, new SubProgressMonitor( monitor, 1 ) );
+        } catch( CoreException cExc ) {
+          status = new Status( IStatus.ERROR,
+                               Activator.PLUGIN_ID,
+                               IStatus.OK,
+                               "Unable to open output stream for " + toStore.getName(),
+                               cExc );
+        }
+      }
+      
+      // Prepare copy operation
+      if ( status.isOK() ) {
         
         byte[] buffer = new byte[ 1024 ];
         SubProgressMonitor subMonitor = new SubProgressMonitor( monitor, 8 );
         subMonitor.beginTask( "Copying " + from.getName(), (int)(length/buffer.length) );
         
+        // Copy the data
         int kb_counter = 0;
-        while ( !subMonitor.isCanceled() ) {
-          int bytesRead = inStream.read( buffer );
+        while ( !subMonitor.isCanceled() && status.isOK() ) {
+          
+          int bytesRead = -1;
+          
+          // Read data from source
+          try {
+            bytesRead = inStream.read( buffer );
+          } catch( IOException ioExc ) {
+            status = new Status( IStatus.ERROR,
+                                 Activator.PLUGIN_ID,
+                                 IStatus.OK,
+                                 "Error while reading data from " + from.getName(),
+                                 ioExc );
+          }
+          
+          // Check if there is still data available
           if ( bytesRead == -1 ) {
             break;
           }
-          outStream.write( buffer, 0, bytesRead );
+          
+          // Write data to target
+          try {
+            outStream.write( buffer, 0, bytesRead );
+          } catch( IOException ioExc ) {
+            status = new Status( IStatus.ERROR,
+                                 Activator.PLUGIN_ID,
+                                 IStatus.OK,
+                                 "Error while writing data to " + toStore.getName(),
+                                 ioExc );
+          }
+          
+          kb_counter++;
           subMonitor.worked( 1 );
-          subMonitor.subTask( fromInfo.getName() + " (" + ( kb_counter++ ) + "/" + (int)(length/buffer.length) + "kB)" );
+          subMonitor.subTask( fromInfo.getName()
+                              + ": "
+                              + (int)(100./length*buffer.length*kb_counter)
+                              + "% ("
+                              + kb_counter
+                              + "/"
+                              + (int)(length/buffer.length)
+                              + "kB)" );
+          
         }
         
-      } else {
-        // TODO mathias
       }
       
     } finally {
+      
+      // Close output stream
       if ( outStream != null ) {
-        outStream.close();
+        try {
+          outStream.close();
+        } catch ( IOException ioExc ) {
+          // just ignore this
+        }
       }
+      
+      // Close input stream
       if ( inStream != null ) {
-        inStream.close();
+        try {
+          inStream.close();
+        } catch( IOException e ) {
+          // just ignore this
+        }
       }
+      
       monitor.done();
+      
     }
+    
+    return status;
         
   }
   
-  private void delete( final IFileStore store,
-                       final IProgressMonitor monitor ) {
+  private IStatus delete( final IFileStore store,
+                          final IProgressMonitor monitor ) {
+    
+    IStatus status = Status.OK_STATUS;
+    
+    try {
+      store.delete( EFS.NONE, monitor );
+    } catch( CoreException cExc ) {
+      status = new Status( IStatus.ERROR,
+                           Activator.PLUGIN_ID,
+                           IStatus.OK,
+                           "Error while deleting " + store.getName(),
+                           cExc );
+    }
+    
+    return status;
     
   }
   
-  private void transferElement( final IGridElement element,
-                                final IGridContainer target,
-                                final IProgressMonitor monitor )
-      throws CoreException {
+  private IStatus transferElement( final IGridElement element,
+                                   final IGridContainer target,
+                                   final IProgressMonitor monitor ) {
+
+    // Prepare variables
+    IStatus status = Status.OK_STATUS;
+    IFileStore inStore = null;
+    IFileStore outStore = null;
     
-    IFileStore inStore = getFileStore( element );
-    IFileStore outStore = getFileStore( target );
-    
-    monitor.beginTask( "Transfering " + element.getName(), 10 );
+    // Get input file store
     try {
-      IProgressMonitor subMonitor = new SubProgressMonitor( monitor, this.move ? 7 : 10 );
-      copy( inStore, outStore, subMonitor );
-      if ( this.move ) {
-        delete( inStore, new SubProgressMonitor( monitor, 3 ) );
-      }
-    } finally {
-      monitor.done();
+      inStore = getFileStore( element );
+    } catch ( CoreException cExc ) {
+      status = new Status( IStatus.ERROR,
+                           Activator.PLUGIN_ID,
+                           IStatus.OK,
+                           "Unable to get file store for " + element.getName(),
+                           cExc );
     }
+    
+    if ( status.isOK() ) {
+      try {
+        outStore = getFileStore( target );
+      } catch ( CoreException cExc ) {
+        status = new Status( IStatus.ERROR,
+                             Activator.PLUGIN_ID,
+                             IStatus.OK,
+                             "Unable to get file store for " + target.getName(),
+                             cExc );
+      }
+    }
+    
+    if ( status.isOK() ) {
+      
+      monitor.beginTask( "Transfering " + element.getName(), 10 );
+      
+      // Put in try-finally-clause to ensure that monitor.done() is called
+      try {
+        
+        // Copy operation
+        IProgressMonitor subMonitor = new SubProgressMonitor( monitor, this.move ? 5 : 9 );
+        status = copy( inStore, outStore, subMonitor );
+        
+        if ( status.isOK() ) {
+          
+          try {
+            target.refresh( new SubProgressMonitor( monitor, 1 ) );
+          } catch( GridModelException gmExc ) {
+            // refresh errors should not disturb the operation
+            // but should be tracked, therefore just log it
+            Activator.logException( gmExc );
+          }
+          
+          if ( monitor.isCanceled() ) {
+            throw new OperationCanceledException();
+          }
+          
+          if ( this.move ) {
+            
+            status = delete( inStore, new SubProgressMonitor( monitor, 3 ) );
+            
+            if ( status.isOK() ) {
+              try {
+                element.getParent().refresh( new SubProgressMonitor( monitor, 1 ) );
+              } catch( GridModelException gmExc ) {
+                // refresh errors should not disturb the operation
+                // but should be tracked, therefore just log it
+                Activator.logException( gmExc );
+              }
+            }
+            
+          }
+          
+        }
+        
+      } finally {
+        monitor.done();
+      }
+      
+    }
+    
+    return status;
     
   }
   
