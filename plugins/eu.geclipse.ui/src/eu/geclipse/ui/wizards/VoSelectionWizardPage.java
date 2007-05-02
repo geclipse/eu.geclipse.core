2236af1fd7674475746fa2375ca03d2afee01a8a
Mathias Stümpert <mathias.stuempert@iwr.fzk.de>
2007-05-02 11:45:10 +0000

    #185082: The next button is now enabled after creating a new VO

--- plugins/eu.geclipse.ui/src/eu/geclipse/ui/wizards/VoSelectionWizardPage.java
+++ plugins/eu.geclipse.ui/src/eu/geclipse/ui/wizards/VoSelectionWizardPage.java
@@ -90,6 +90,7 @@ public class VoSelectionWizardPage extends WizardPage {
         createNewVO();
         updateVoList();
         showSelectedInfo();
+        setPageComplete( getSelectedVo() != null );
       }
     } );
     
