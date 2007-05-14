80944f38a86f410990b55c98995c474dcef5673c
Harald Kornmayer <harald@kornmayer.eu>
2007-05-14 09:56:29 +0000

    Added the missing copy rights section at the beginning of the files.

--- plugins/eu.geclipse.webview/src/eu/geclipse/webview/preferences/Messages.java
+++ plugins/eu.geclipse.webview/src/eu/geclipse/webview/preferences/Messages.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2006 g-Eclipse Consortium.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Harald Kornmayer - initial implementation
+ *******************************************************************************/
+
 package eu.geclipse.webview.preferences;
 
 import java.util.MissingResourceException;
