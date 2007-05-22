576e2db7f6780ad6cd9b516b3722b2f661539941
Mathias Stümpert <mathias.stuempert@iwr.fzk.de>
2007-05-22 13:09:37 +0000

    #188296: bug fixed

--- plugins/eu.geclipse.ui/src/eu/geclipse/ui/providers/JobViewLabelProvider.java
+++ plugins/eu.geclipse.ui/src/eu/geclipse/ui/providers/JobViewLabelProvider.java
@@ -26,7 +26,9 @@ public class JobViewLabelProvider
           break;
         case 3:
           IGridJobStatus status = job.getJobStatus();
-          text = status.getName();
+          if ( status != null ) {
+            text = status.getName();
+          }
           break;
       }
     }
