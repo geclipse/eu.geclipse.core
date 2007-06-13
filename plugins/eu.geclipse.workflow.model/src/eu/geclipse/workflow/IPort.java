c4ba8f415d8730d54c655f9cf2ed3b9e4a1d8d9b
Ashish Thandavan <athandava>
2007-06-13 11:01:22 +0000

    Initial import of eu.geclipse.workflow.model plugin.

---
+++ plugins/eu.geclipse.workflow.model/src/eu/geclipse/workflow/IPort.java
@@ -0,0 +1,30 @@
+/*****************************************************************************
+ * Copyright (c) 2006, 2007 g-Eclipse Consortium 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Initial development of the original code was made for the
+ * g-Eclipse project founded by European Union
+ * project number: FP6-IST-034327  http://www.geclipse.eu/
+ *
+ * Contributors:
+ *    Ashish Thandavan - initial API and implementation
+ *****************************************************************************/
+package eu.geclipse.workflow;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>IPort</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see eu.geclipse.workflow.IWorkflowPackage#getIPort()
+ * @model abstract="true"
+ * @generated
+ */
+public interface IPort extends IWorkflowElement
+{
+} // IPort
