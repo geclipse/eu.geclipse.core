d3df9735c698642580cd6553c65b783265b7eef9
Pawel Wolniewicz <pwolniewi>
2007-06-16 20:48:58 +0000

    implementation of jobStatusListener support

--- plugins/eu.geclipse.core/src/eu/geclipse/core/JobStatusUpdater.java
+++ plugins/eu.geclipse.core/src/eu/geclipse/core/JobStatusUpdater.java
@@ -1,25 +1,55 @@
+/*****************************************************************************
+ * Copyright (c) 2006, 2007 g-Eclipse Consortium 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Initial development of the original code was made for the
+ * g-Eclipse project founded by European Union
+ * project number: FP6-IST-034327  http://www.geclipse.eu/
+ *
+ * Contributors:
+ *    Pawel Wolniewicz 
+ *****************************************************************************/
 package eu.geclipse.core;
 
+import java.util.Enumeration;
 import java.util.Hashtable;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
 import eu.geclipse.core.internal.Activator;
+import eu.geclipse.core.internal.model.JobManager;
 import eu.geclipse.core.model.IGridJob;
-import eu.geclipse.core.model.IGridJobID;
 import eu.geclipse.core.model.IGridJobStatus;
+import eu.geclipse.core.model.IGridJobStatusListener;
 
+/**
+ * The class for updating job status in the background. There is one JobStatusUpdater 
+ * for each job and tt is started when a job is added to project.  
+ * @see JobManager
+ * @see IGridJobStatus
+ * @see IGridJob
+ *
+ */
 public class JobStatusUpdater extends Job {
 
-  static Hashtable<String, JobStatusUpdater> updaters = new Hashtable<String, JobStatusUpdater>();
+  /**
+   * Hashtable that hold infomation about registered listeners and the statuses for
+   * which listener should be notified
+   * @seeIGridJobStatusListener
+   */
+  Hashtable<IGridJobStatusListener, Integer> listeners=new Hashtable<IGridJobStatusListener, Integer>();
+//  static Hashtable<String, JobStatusUpdater> updaters = new Hashtable<String, JobStatusUpdater>();
   
   IGridJob job;
 
   public JobStatusUpdater( final IGridJob job ) {
     super( "Grid Job Status Updater" );
     this.job = job;
-    updaters.put( job.getID().getJobID(), this );
+//    updaters.put( job.getID().getJobID(), this );
   }
 
   @Override
@@ -38,7 +68,14 @@ public class JobStatusUpdater extends Job {
         newType=status.getType();
       }
       if( oldType != newType ) {
-      // TODO pawelw - status changed, notify all listeners;
+      // status changed, notify all listeners;
+      for(Enumeration<IGridJobStatusListener> e=listeners.keys();e.hasMoreElements();){
+        IGridJobStatusListener listener =e.nextElement();
+        int trigger=listeners.get( listener );
+        if( (newType & trigger)>0){
+          listener.statusChanged( job );
+        }
+      }
     }
     }
     catch(RuntimeException e){
@@ -48,25 +85,29 @@ public class JobStatusUpdater extends Job {
       schedule( 10000 );
     }
     else{
-      updaters.remove( this.job.getID().getJobID() );
+      JobManager.getManager().removeUpdater( this );
     }
     return Status.OK_STATUS;
   }
   
-  static void waitForJob(final String id) throws InterruptedException, IllegalAccessException{
-    JobStatusUpdater jsu = updaters.get( id );
-    if(jsu==null){
-      throw new IllegalAccessException("No updater for job id: "+id);
-    }
-    jsu.join();
+
+  /**
+   * Add status listener for the updater. The listener will be notify, when the status
+   * of the job will change. 
+   * @param status - aggregate value of IGridJobStatus types, for which listener 
+   * should be notified.
+   * @param listener - listener to be notifies about the change.
+   */
+  public void addJobStatusListener( final int status, final IGridJobStatusListener listener ) {
+    listeners.put( listener, status );
   }
 
-  static void waitForJob(final IGridJobID id) throws InterruptedException, IllegalAccessException{
-    waitForJob(id.getJobID());
-  }
-
-  static void waitForJob(final IGridJob job) throws InterruptedException, IllegalAccessException{
-    waitForJob(job.getID().getJobID());
+  /**
+   * Removes registrered job status listener
+   * @param listener
+   */
+  public void removeJobStatusListener( final IGridJobStatusListener listener ) {
+    listeners.remove( listener );
   }
 
 }
--- plugins/eu.geclipse.core/src/eu/geclipse/core/internal/model/JobManager.java
+++ plugins/eu.geclipse.core/src/eu/geclipse/core/internal/model/JobManager.java
@@ -11,12 +11,15 @@
  *
  * Contributors:
  *    Mathias Stuempert - initial API and implementation
+ *    Pawel Wolniewicz 
  *****************************************************************************/
-
 package eu.geclipse.core.internal.model;
 
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.Hashtable;
 import java.util.List;
-
+import java.util.NoSuchElementException;
 import eu.geclipse.core.JobStatusUpdater;
 import eu.geclipse.core.model.GridModelException;
 import eu.geclipse.core.model.IGridElement;
@@ -27,37 +30,42 @@ import eu.geclipse.core.model.IGridJobStatusListener;
 /**
  * Core implementation of an {@link IGridJobManager}.
  */
-public class JobManager
-    extends AbstractGridElementManager
-    implements IGridJobManager {
-  
+public class JobManager extends AbstractGridElementManager
+  implements IGridJobManager
+{
+
   /**
    * The name of this manager.
    */
   private static final String NAME = ".jobs"; //$NON-NLS-1$
-  
   /**
    * The singleton.
    */
   private static JobManager singleton;
-    
   /**
-   * Private constructor to ensure to have only one instance of
-   * this class. This can be obtained by {@link #getManager()}.
+   * Hashtable for holding information about updaters assigned to jobs.
+   */
+  private Hashtable<IGridJob, JobStatusUpdater> updaters = new Hashtable<IGridJob, JobStatusUpdater>();
+
+  /**
+   * Private constructor to ensure to have only one instance of this class. This
+   * can be obtained by {@link #getManager()}.
    */
   private JobManager() {
     // empty imlementation
   }
-  
+
   @Override
   public boolean addElement( final IGridElement element )
-      throws GridModelException {
+    throws GridModelException
+  {
     boolean flag;
-    flag=super.addElement( element );
-    if(element instanceof IGridJob){
-      JobStatusUpdater updater = new JobStatusUpdater ((IGridJob)element);
+    flag = super.addElement( element );
+    if( element instanceof IGridJob ) {
+      JobStatusUpdater updater = new JobStatusUpdater( ( IGridJob )element );
+      updaters.put( ( IGridJob )element, updater );
       updater.setSystem( true );
-      updater.schedule(20000);
+      updater.schedule( 20000 );
     }
     return flag;
   }
@@ -68,40 +76,104 @@ public class JobManager
    * @return The singleton.
    */
   public static JobManager getManager() {
-    if ( singleton == null ) {
+    if( singleton == null ) {
       singleton = new JobManager();
     }
     return singleton;
   }
 
-  /* (non-Javadoc)
+  /*
+   * (non-Javadoc)
+   * 
    * @see eu.geclipse.core.internal.model.AbstractGridElementManager#getName()
    */
   public String getName() {
     return NAME;
   }
 
-  /* (non-Javadoc)
+  /*
+   * (non-Javadoc)
+   * 
    * @see eu.geclipse.core.model.IGridElementManager#canManage(eu.geclipse.core.model.IGridElement)
    */
   public boolean canManage( final IGridElement element ) {
     return element instanceof IGridJob;
   }
 
-  /* (non-Javadoc)
-   * @see eu.geclipse.core.model.IGridJobManager#addJobStatusListener(java.util.List, int, eu.geclipse.core.model.IGridJobStatusListener)
+  /*
+   * (non-Javadoc)
+   * 
+   * @see eu.geclipse.core.model.IGridJobManager#addJobStatusListener(java.util.List,
+   *      int, eu.geclipse.core.model.IGridJobStatusListener)
    */
-  public void addJobStatusListener(List<IGridJob> jobs, int status, IGridJobStatusListener listener) {
-    // TODO pawelw
-    
+  public void addJobStatusListener( final List<IGridJob> jobs,
+                                    final int status,
+                                    final IGridJobStatusListener listener )
+  {
+    JobStatusUpdater updater;
+    for( IGridJob job : jobs ) {
+      updater = ( JobStatusUpdater )updaters.get( job );
+      updater.addJobStatusListener( status, listener );
+    }
   }
 
-  /* (non-Javadoc)
+  /*
+   * (non-Javadoc)
+   * 
    * @see eu.geclipse.core.model.IGridJobManager#removeJobStatusListener(eu.geclipse.core.model.IGridJobStatusListener)
    */
-  public void removeJobStatusListener(IGridJobStatusListener listener) {
-    // TODO pawelw
-    
+  public void removeJobStatusListener( final IGridJobStatusListener listener ) {
+    JobStatusUpdater updater;
+    for( Enumeration<JobStatusUpdater> e = updaters.elements(); e.hasMoreElements(); )
+    {
+      updater = e.nextElement();
+      updater.removeJobStatusListener( listener );
+    }
   }
+
+  /**
+   * Removes updater from JobManager. It is called by updater, when it finished
+   * it works (job status cannot change any more)
+   * 
+   * @param updater
+   */
+  public void removeUpdater( final JobStatusUpdater updater ) {
+    Collection<JobStatusUpdater> values = updaters.values();
+    while( values.remove( updater ) ) {
+      // empty block};
+    }
+  }
+
+  // void waitForJob(final String id) throws InterruptedException,
+  // IllegalAccessException{
+  // JobStatusUpdater jsu = updaters.get( id );
+  //
+  // if(jsu==null){
+  // throw new IllegalAccessException("No updater for job id: "+id);
+  // }
+  // jsu.join();
+  // }
+  //
+  // void waitForJob(final IGridJobID id) throws InterruptedException,
+  // IllegalAccessException{
+  // waitForJob(id.getJobID());
+  // }
+
   
+  /**
+   * Wait until updater for the given job finishes, which means that the job status 
+   * is final and cannot be changed any more.
+   * @param job
+   * @throws InterruptedException
+   * @throws NoSuchElementException
+   */
+  void waitForJob( final IGridJob job )
+    throws InterruptedException, NoSuchElementException
+  {
+    JobStatusUpdater updater = updaters.get( job );
+    if( updater == null ) {
+      throw new NoSuchElementException();
+    }
+    updater.join();
+  }
 }
