92402983fed8ba32527c2cab4f2a6796041cb3a2
Mathias Stümpert <mathias.stuempert@iwr.fzk.de>
2007-06-21 11:19:13 +0000

    #193555: patch applied

--- plugins/eu.geclipse.ui/src/eu/geclipse/ui/internal/OperatorPerspectiveFactory.java
+++ plugins/eu.geclipse.ui/src/eu/geclipse/ui/internal/OperatorPerspectiveFactory.java
@@ -30,36 +30,53 @@ public class OperatorPerspectiveFactory implements IPerspectiveFactory {
     
     String editorArea = layout.getEditorArea();
 
-    IFolderLayout leftLeftFolder
-      = layout.createFolder( "leftleft", IPageLayout.LEFT, 0.5f, editorArea ); //$NON-NLS-1$
-    leftLeftFolder.addView( eu.geclipse.ui.internal.Activator.ID_CONNECTION_VIEW );
-    leftLeftFolder.addPlaceholder( IPageLayout.ID_RES_NAV );
-    
-    IFolderLayout leftRightFolder
-      = layout.createFolder( "leftright", IPageLayout.RIGHT, 0.5f, eu.geclipse.ui.internal.Activator.ID_CONNECTION_VIEW ); //$NON-NLS-1$
-    leftRightFolder.addView( eu.geclipse.ui.internal.Activator.ID_GLUE_INFO_VIEW );
+    IFolderLayout topLeftFolder
+     = layout.createFolder( "topleft", IPageLayout.LEFT, 0.2f, editorArea ); //$NON-NLS-1$
+    topLeftFolder.addView( eu.geclipse.ui.internal.Activator.ID_GPROJECT_VIEW );
+    topLeftFolder.addPlaceholder( IPageLayout.ID_RES_NAV );
+
+    IFolderLayout bottomLeftFolder
+     = layout.createFolder( "bottomleft", IPageLayout.BOTTOM, 0.5f, eu.geclipse.ui.internal.Activator.ID_GPROJECT_VIEW ); //$NON-NLS-1$
+    bottomLeftFolder.addView( eu.geclipse.ui.internal.Activator.ID_GLUE_INFO_VIEW );
+   
+    IFolderLayout rightFolder
+      = layout.createFolder( "right", IPageLayout.RIGHT, 0.5f, eu.geclipse.ui.internal.Activator.ID_TERMINAL_VIEW ); //$NON-NLS-1$
+    rightFolder.addView( eu.geclipse.ui.internal.Activator.ID_TERMINAL_VIEW );
       
     IFolderLayout bottomFolder
-      = layout.createFolder( "bottom", IPageLayout.BOTTOM, 0.75f, editorArea ); //$NON-NLS-1$
-    bottomFolder.addView( eu.geclipse.ui.internal.Activator.ID_TERMINAL_VIEW );
+      = layout.createFolder( "bottom", IPageLayout.BOTTOM, 0.6f, editorArea ); //$NON-NLS-1$
+    bottomFolder.addView( IPageLayout.ID_PROP_SHEET );
+    // TODO This should have been the following identifier. 
+    // eu.geclipse.batch.ui.internal.Activator.ID_BATCH_JOB_VIEW
+    // Due to batch.ui plugin location have to do this hack
+    // Leve as Warning to remember
+    bottomFolder.addView( "eu.geclipse.batch.ui.views.BatchJobView" );
     bottomFolder.addView( eu.geclipse.ui.internal.Activator.ID_AUTH_VIEW );
+    
+    bottomFolder.addPlaceholder( IPageLayout.ID_PROP_SHEET );
     bottomFolder.addPlaceholder( IPageLayout.ID_PROBLEM_VIEW );
     bottomFolder.addPlaceholder( NewSearchUI.SEARCH_VIEW_ID );
     bottomFolder.addPlaceholder( IPageLayout.ID_BOOKMARKS );
     bottomFolder.addPlaceholder( IProgressConstants.PROGRESS_VIEW_ID );
     
+    layout.addShowViewShortcut( "eu.geclipse.batch.ui.views.BatchJobView" );
+    layout.addShowViewShortcut( IPageLayout.ID_PROP_SHEET );
     layout.addShowViewShortcut( IPageLayout.ID_PROBLEM_VIEW );
     layout.addShowViewShortcut( IPageLayout.ID_RES_NAV );
     layout.addShowViewShortcut( IPageLayout.ID_TASK_LIST );
     layout.addShowViewShortcut( IProgressConstants.PROGRESS_VIEW_ID );
 
+    // TODO This should have been the following identifier. 
+    // eu.geclipse.batch.ui.internal.Activator.ID_BATCH_WIZARD
+    // Due to batch.ui plugin location have to do this hack
+    // Leve as Warning to remember
+    layout.addNewWizardShortcut( "eu.geclipse.batch.ui.wizards.BatchCreationWizard" );
     layout.addNewWizardShortcut( eu.geclipse.ui.internal.Activator.ID_PROJECT_WIZARD );
     layout.addNewWizardShortcut( eu.geclipse.ui.internal.Activator.ID_CONNECTION_WIZARD );
     layout.addNewWizardShortcut( eu.geclipse.ui.internal.Activator.ID_WORKFLOW_WIZARD );
     layout.addNewWizardShortcut( "org.eclipse.ui.wizards.new.folder" );//$NON-NLS-1$
     layout.addNewWizardShortcut( "org.eclipse.ui.wizards.new.file" );//$NON-NLS-1$
     layout.addNewWizardShortcut( "org.eclipse.ui.editors.wizards.UntitledTextFileWizard" );//$NON-NLS-1$
-    
   }
 
 }
