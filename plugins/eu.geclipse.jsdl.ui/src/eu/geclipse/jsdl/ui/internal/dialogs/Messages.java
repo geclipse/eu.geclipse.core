dc7fcdbfdde02e83ee7ff137be6d277515a6cd3a
Katarzyna Bylec <katarzyna.bylec@gmail.com>
2007-07-26 12:29:13 +0000

    Code clean-up

--- plugins/eu.geclipse.jsdl.ui/src/eu/geclipse/jsdl/ui/internal/dialogs/Messages.java
+++ plugins/eu.geclipse.jsdl.ui/src/eu/geclipse/jsdl/ui/internal/dialogs/Messages.java
@@ -1,10 +1,26 @@
+/******************************************************************************
+ * Copyright (c) 2007 g-Eclipse consortium 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Initial development of the original code was made for
+ * project g-Eclipse founded by European Union
+ * project number: FP6-IST-034327  http://www.geclipse.eu/
+ *
+ * Contributor(s):
+ *     PSNC: 
+ *      - Katarzyna Bylec (katis@man.poznan.pl)
+ *           
+ *****************************************************************************/
 package eu.geclipse.jsdl.ui.internal.dialogs;
 
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 /**
- * Returns the localised messages for this package.
+ * Returns the messages for this package.
  */
 public class Messages {
 
