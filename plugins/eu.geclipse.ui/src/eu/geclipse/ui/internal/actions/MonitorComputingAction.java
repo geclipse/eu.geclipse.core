890c9887cf88cfb61bb011d902008372414a5802
Martin Polak <mpolak@gup.jku.at>
2007-08-23 11:04:22 +0000

    updated for CE selection

--- plugins/eu.geclipse.ui/src/eu/geclipse/ui/internal/actions/MonitorComputingAction.java
+++ plugins/eu.geclipse.ui/src/eu/geclipse/ui/internal/actions/MonitorComputingAction.java
@@ -1,17 +1,44 @@
+/*****************************************************************************
+ * Copyright (c) 2006, 2007 g-Eclipse Consortium 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Initial development of the original code was made for the
+ * g-Eclipse project founded by European Union
+ * project number: FP6-IST-034327  http://www.geclipse.eu/
+ *
+ * Contributors:
+ *    Martin Polak GUP, JKU - initial implementation
+ *****************************************************************************/
 package eu.geclipse.ui.internal.actions;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
-
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchSite;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.actions.SelectionListenerAction;
-
 import eu.geclipse.core.model.IGridComputing;
+import eu.geclipse.core.model.IGridJob;
+import eu.geclipse.core.model.IGridStorage;
 import eu.geclipse.ui.internal.Activator;
+import eu.geclipse.ui.views.ProcessStatView;
+import eu.geclipse.ui.views.jobdetails.JobDetailsView;
 
+/**
+ * Contributes an action to the context menu of a GridProject that allows the user
+ * to select a computing resource to be monitored and add it to the ProcessStatView. 
+ * @author mpolak
+ * 
+ */
 public class MonitorComputingAction extends SelectionListenerAction {
   /**
    * The workbench site this action belongs to.
@@ -47,22 +74,29 @@ public class MonitorComputingAction extends SelectionListenerAction {
   public void run()
   {
     this.selection = this.getStructuredSelection();
+    ProcessStatView pviewer = null;
 
     Object element
-    = getStructuredSelection().getFirstElement();
+    = this.selection.getFirstElement();
     if ( ( element != null ) && ( element instanceof IGridComputing ) ) {
       IGridComputing remote = ( IGridComputing ) element;
       try {
-        this.site.getPage().showView( Activator.ID_PROCESS_STATUS,
-                                     "Hallo",
+        pviewer = (ProcessStatView)this.site.getPage().showView( Activator.ID_PROCESS_STATUS,
+                                     "Monitoring Your CE",
                                      IWorkbenchPage.VIEW_ACTIVATE );
         
-        // get reference to the current active View and add the VIEW to it
+        
+        URI targeturi = new URI( "gsiftp://"+remote.getName()); //$NON-NLS-1$
+        URI connecturi = new URI(targeturi.getScheme(),targeturi.getUserInfo(),targeturi.getHost(), 2811,null, null, null); //$NON-NLS-1$
+        pviewer.addSite(connecturi);
+        
       } catch( PartInitException e ) {
         // Just ignore this exception and do not open the job
+      } catch( URISyntaxException e ) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
       }
-      //ProcessStatView remote = this.site.getWorkbenchWindow().
-      //add new monitored site
+      
     }
   }
 
