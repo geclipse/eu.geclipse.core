af9768687eefe5d34e1dd1c1e9eae0c6b96e29cf
Mariusz Wojtysiak <mwojtysia>
2007-08-29 10:35:55 +0000

    198377 IGridJobStatusListener.statusChanged() throws "Invalid thread access" during calling SWT

--- plugins/eu.geclipse.core/src/eu/geclipse/core/model/IGridJobStatusListener.java
+++ plugins/eu.geclipse.core/src/eu/geclipse/core/model/IGridJobStatusListener.java
@@ -14,11 +14,22 @@
  *****************************************************************************/
 package eu.geclipse.core.model;
 
+/**
+ * Interface for listener called when job status is updated
+ */
 public interface IGridJobStatusListener {
 
   /**
+   * Called when job status is changed. Warning! Can be called from working
+   * thread, so please from statusChanged() don't call SWT methods directly. 
+   * Rather use:
+   * use: 
+   *    display.syncExec ( new Runnable () { 
+   *        public void run () { } 
+   *        } );
+   * 
    * @param job
    */
-  public void statusChanged(IGridJob job);
+  public void statusChanged( IGridJob job );
   
 }
