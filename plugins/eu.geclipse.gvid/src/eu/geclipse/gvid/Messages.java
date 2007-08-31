1a6bec5d0d60c1cc40d0b22e49e9b8bfb93693f1
Thomas Köckerbauer <tkoeck@gup.jku.at>
2007-08-31 10:46:17 +0000

    added copyrights

--- plugins/eu.geclipse.gvid/src/eu/geclipse/gvid/Messages.java
+++ plugins/eu.geclipse.gvid/src/eu/geclipse/gvid/Messages.java
@@ -1,8 +1,26 @@
+/*****************************************************************************
+ * Copyright (c) 2006, 2007 g-Eclipse Consortium 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Initial development of the original code was made for the
+ * g-Eclipse project founded by European Union
+ * project number: FP6-IST-034327  http://www.geclipse.eu/
+ *
+ * Contributors:
+ *    Thomas Koeckerbauer GUP, JKU - initial API and implementation
+ *****************************************************************************/
+
 package eu.geclipse.gvid;
 
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
+/**
+ * Returns the localised messages for this package.
+ */
 public class Messages {
   private static final String BUNDLE_NAME = "eu.geclipse.gvid.messages"; //$NON-NLS-1$
 
@@ -10,13 +28,22 @@ public class Messages {
       .getBundle(BUNDLE_NAME);
 
   private Messages() {
+    // not instanceable
   }
 
+  /**
+   * Returns a localised version of a message.
+   * 
+   * @param key key for the message.
+   * @return the localised string.
+   */
   public static String getString( final String key ) {
+    String message;
     try {
-      return RESOURCE_BUNDLE.getString( key );
-    } catch( MissingResourceException e ) {
-      return '!' + key + '!';
+      message = RESOURCE_BUNDLE.getString( key );
+    } catch( final MissingResourceException exception ) {
+      message = '!' + key + '!';
     }
+    return message;
   }
 }
