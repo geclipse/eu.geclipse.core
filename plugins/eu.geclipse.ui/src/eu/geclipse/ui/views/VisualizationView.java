876e1f2ebc3e2a22c292812e00d08204f365b7c4
Sylva Girtelschmid <sgirtel@gup.jku.at>
2007-10-29 11:03:04 +0000

    Adding the missing copyright.

--- plugins/eu.geclipse.ui/src/eu/geclipse/ui/views/VisualizationView.java
+++ plugins/eu.geclipse.ui/src/eu/geclipse/ui/views/VisualizationView.java
@@ -1,3 +1,17 @@
+/*****************************************************************************
+ * Copyright (c) 2006, 2007 g-Eclipse Consortium 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Initial development of the original code was made for the
+ * g-Eclipse project founded by European Union
+ * project number: FP6-IST-034327  http://www.geclipse.eu/
+ *
+ * Contributors:
+ *    Sylva Girtelschmid GUP, JKU - initial API and implementation
+ *****************************************************************************/
 package eu.geclipse.ui.views;
 
 import org.eclipse.swt.SWT;
@@ -9,11 +23,7 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.ViewPart;
 
-import eu.geclipse.core.CoreProblems;
-import eu.geclipse.core.GridException;
 import eu.geclipse.core.model.IGridVisualization;
-import eu.geclipse.ui.dialogs.NewProblemDialog;
-import eu.geclipse.ui.internal.actions.Messages;
 
 
 /**
