b0e33ddadaab458e4d52425ebe305140485d6286
Nicholas Loulloudes <loulloudes.n@cs.ucy.ac.cy>
2007-11-09 14:36:46 +0000

    Add missing copyright header to GlueUtility.java

--- plugins/eu.geclipse.info/src/eu/geclipse/info/glue/GlueUtility.java
+++ plugins/eu.geclipse.info/src/eu/geclipse/info/glue/GlueUtility.java
@@ -1,3 +1,18 @@
+/*****************************************************************************
+ * Copyright (c) 2006, 2007 g-Eclipse Consortium 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Initial development of the original code was made for the
+ * g-Eclipse project founded by European Union
+ * project number: FP6-IST-034327  http://www.geclipse.eu/
+ *
+ * Contributors:
+ *    Nikolaos Tsioutsias - initial API and implementation
+ *    
+ *****************************************************************************/
 package eu.geclipse.info.glue;
 
 import javax.naming.NamingException;
@@ -5,12 +20,21 @@ import javax.naming.directory.Attribute;
 import javax.naming.directory.Attributes;
 
 
+/**
+ * @author nickl
+ *
+ */
 public class GlueUtility {
   
+  /**
+   * @param attribute
+   * @param attributes
+   * @return
+   */
   public static String getStringAttribute( final String attribute,
                                            final Attributes attributes )
   {
-    String result = "";
+    String result = ""; //$NON-NLS-1$
     Attribute attr = attributes.get( attribute );
     if( attr != null ) {
       try {
@@ -22,6 +46,11 @@ public class GlueUtility {
     return result;
   }
 
+  /**
+   * @param attribute
+   * @param attributes
+   * @return
+   */
   public static int getIntegerAttribute( final String attribute,
                                    final Attributes attributes )
   {
@@ -37,6 +66,11 @@ public class GlueUtility {
     return result;
   }
 
+  /**
+   * @param attribute
+   * @param attributes
+   * @return
+   */
   public static long getLongAttribute( final String attribute,
                                        final Attributes attributes )
   {
