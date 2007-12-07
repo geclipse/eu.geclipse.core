e9cff8221356c161c717aa1d68e53dee3575590e
Katarzyna Bylec <katarzyna.bylec@gmail.com>
2007-12-07 07:56:24 +0000

    Changes in IGridTest

--- plugins/eu.geclipse.core/src/eu/geclipse/core/model/IGridTest.java
+++ plugins/eu.geclipse.core/src/eu/geclipse/core/model/IGridTest.java
@@ -16,52 +16,82 @@
  *****************************************************************************/
 package eu.geclipse.core.model;
 
-import java.util.Calendar;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-
-import org.eclipse.core.runtime.jobs.IJobStatus;
-
-
+import java.util.Map;
 
 /**
  * Interface for structural tests and simple tests.
  */
 public interface IGridTest extends IGridElement, IManageable {
-  
-  public enum TestType{
-    SAM,
-    JOB,
-    OTHER,
-    SINGLE, 
-    CERT
-  }
-  
+
+  /**
+   * Method to access all single tests that are run as a one IGridTest
+   * implementation.
+   * 
+   * @return list of single tests names
+   */
+  public List<String> getSingleTestsNames();
+
+  /**
+   * Method to access names of tested resources.
+   * 
+   * @return List of names of tested resources
+   */
+  public List<String> getTestedResourcesNames();
+
+  /**
+   * Method to access dates of all submissions of this test
+   * 
+   * @return List of dates of test submission
+   */
+  public List<Date> getTestSubmissionDates();
+
+  /**
+   * Method to access text interpretation of single test for given single test's
+   * name, tested resources and date of this test.
+   * 
+   * @param testName name of single test
+   * @param resourceName name of tested resources
+   * @param date date of test submission
+   * @return String that is representation of single test result
+   */
+  public String getTestSingleTestResult( final String testName,
+                                         final String resourceName,
+                                         final Date date );
+
+  /**
+   * Short user friendly description of this test.
+   * 
+   * @return String with test's description
+   */
+  public String getTestDescription();
+
   /**
    * Method returning test name
+   * 
    * @return String
    */
   public String getName();
-  
+
   /**
-   * Method returning type of the test
-   * @return TestType
-   */
-  public TestType getType();
-  
-  /**
-   * Method returning result of the test
-   * @return
+   * Method returning result of the test - as a summary of single tests.
+   * 
+   * @return Test's result
    */
   public Object getResult();
-  
+
   /**
    * Method returns all the data which needs to be viewed in properties view.
-   * @return
+   * 
+   * @return Map with property name as a key and its value as a value
    */
-  public HashMap<String, String> getDescriptions();
+  public Map<String, String> getDescriptions();
 
-  public Date getDate();
-  
+  /**
+   * Method to access date of last test submission
+   * 
+   * @return date of last test submission
+   */
+  public Date getLastUpdate();
 }
