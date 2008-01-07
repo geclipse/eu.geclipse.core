76547f99cba96991d3bebe40ba0cdc370a6f9ad6
Mathias Stümpert <mathias.stuempert@iwr.fzk.de>
2008-01-07 10:13:07 +0000

    adoptions to new problem reporting system

---
+++ plugins/eu.geclipse.ui/src/eu/geclipse/ui/internal/LogExceptionSolution.java
@@ -0,0 +1,54 @@
+package eu.geclipse.ui.internal;
+
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+
+import eu.geclipse.core.reporting.ISolution;
+
+public class LogExceptionSolution
+    implements ISolution {
+  
+  /**
+   * The ID of the PDE log view.
+   */
+  public static final String LOG_VIEW_ID
+    = "org.eclipse.pde.runtime.LogView"; //$NON-NLS-1$
+  
+  private Throwable exception;
+  
+  public LogExceptionSolution( final Throwable exc ) {
+    this.exception = exc;
+  }
+
+  public String getDescription() {
+    return "Log exception";
+  }
+  
+  public String getID() {
+    return null;
+  }
+
+  public boolean isActive() {
+    return true;
+  }
+
+  public void solve() {
+    
+    Activator.logException( this.exception );
+    
+    IWorkbench workbench = PlatformUI.getWorkbench();
+    IWorkbenchWindow window = workbench.getActiveWorkbenchWindow();
+    IWorkbenchPage page = window.getActivePage();
+  
+    try {
+      page.showView( LOG_VIEW_ID );
+    } catch( PartInitException piExc ) {
+      Activator.logException( piExc );
+    }
+    
+  }
+
+}
---
+++ plugins/eu.geclipse.ui/src/eu/geclipse/ui/internal/MailToSolution.java
@@ -0,0 +1,110 @@
+package eu.geclipse.ui.internal;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.browser.IWebBrowser;
+import org.eclipse.ui.browser.IWorkbenchBrowserSupport;
+
+import eu.geclipse.core.reporting.IProblem;
+import eu.geclipse.core.reporting.ISolution;
+
+public class MailToSolution implements ISolution {
+  
+  private IProblem problem;
+  
+  public MailToSolution( final IProblem problem ) {
+    this.problem = problem;
+  }
+
+  public String getDescription() {
+    return "Send error report";
+  }
+  
+  public String getID() {
+    return null;
+  }
+
+  public boolean isActive() {
+    return ( this.problem != null ) && ( this.problem.getMailTo() != null );
+  }
+
+  public void solve() {
+    URL url = getMailToLink();
+    if ( url != null ) {
+      IWorkbenchBrowserSupport browserSupport
+        = PlatformUI.getWorkbench().getBrowserSupport();
+      try {
+        IWebBrowser externalBrowser
+          = browserSupport.getExternalBrowser();
+        externalBrowser.openURL( url );
+      } catch ( PartInitException piExc ) {
+        Activator.logException( piExc );
+      }
+    }
+  }
+  
+  private String getMailBody( final IProblem problem ) {
+    
+    StringBuffer buffer = new StringBuffer();
+    
+    buffer.append(
+        "Description: " + problem.getDescription() + "%0A%0A" +
+        "Plugin: " + problem.getPluginID() + "%0A%0A"
+    );
+    
+    String[] reasons = problem.getReasons();
+    if ( ( reasons != null ) && ( reasons.length > 0 ) ) {
+      buffer.append( "Reasons:%0A" );
+      for ( String reason : reasons ) {
+        buffer.append( " - " + reason + "%0A" );
+      }
+      buffer.append( "%0A" );
+    }
+    
+    ISolution[] solutions = problem.getSolutions();
+    if ( ( solutions != null ) && ( solutions.length > 0 ) ) {
+      buffer.append( "Solutions:%0A" );
+      for ( ISolution solution : solutions ) {
+        buffer.append( " - " + solution.getDescription() + "%0A" );
+      }
+      buffer.append( "%0A" );
+    }
+    
+    Throwable exc = problem.getException();
+    if ( exc != null ) {
+      buffer.append( "Stacktrace:%0A" );
+      StringWriter sWriter = new StringWriter();
+      PrintWriter pWriter = new PrintWriter( sWriter );
+      exc.printStackTrace( pWriter );
+      pWriter.flush();
+      buffer.append( sWriter.getBuffer() );
+      pWriter.close();
+    }
+    
+    String body = buffer.toString().replaceAll( "[\r\n]", "%0A" );
+    
+    return body;
+    
+  }
+  
+  private URL getMailToLink() {
+    URL result = null;
+    String mailTo = this.problem.getMailTo();
+    try {
+      result = new URL(
+          "mailto:" + mailTo +
+          "?subject=Problem Report: " + this.problem.getDescription() +
+          "&body=" + getMailBody( this.problem )
+      );
+    } catch ( MalformedURLException murlExc ) {
+      Activator.logException( murlExc );
+    }
+    return result;
+  }
+
+}
