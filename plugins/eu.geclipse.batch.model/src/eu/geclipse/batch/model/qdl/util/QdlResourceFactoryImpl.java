38563b87c0e607d222f155c3484a25a3ed0f99bf
Nicholas Loulloudes <loulloudes.n@cs.ucy.ac.cy>
2008-01-11 15:04:34 +0000

    Adding copyrights to model.

--- plugins/eu.geclipse.batch.model/src/eu/geclipse/batch/model/qdl/util/QdlResourceFactoryImpl.java
+++ plugins/eu.geclipse.batch.model/src/eu/geclipse/batch/model/qdl/util/QdlResourceFactoryImpl.java
@@ -1,9 +1,19 @@
-/**
- * <copyright>
- * </copyright>
+/******************************************************************************
+ * Copyright (c) 2008 g-Eclipse consortium
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- * $Id$
- */
+ * Initial development of the original code was made for
+ * project g-Eclipse founded by European Union
+ * project number: FP6-IST-034327  http://www.geclipse.eu/
+ *
+ * Contributor(s):
+ *     UCY (http://www.ucy.cs.ac.cy)
+ *      - Nicholas Loulloudes (loulloudes.n@cs.ucy.ac.cy)
+ *
+  *****************************************************************************/
 package eu.geclipse.batch.model.qdl.util;
 
 import org.eclipse.emf.common.util.URI;
