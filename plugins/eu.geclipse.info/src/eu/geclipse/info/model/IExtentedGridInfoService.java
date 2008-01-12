918ee18f43cc4439a7b2c847db4fddb081bf50a9
Ariel Garcia <agarcia>
2008-01-12 19:44:13 +0000

    Remove unused imports

--- plugins/eu.geclipse.info/src/eu/geclipse/info/model/IExtentedGridInfoService.java
+++ plugins/eu.geclipse.info/src/eu/geclipse/info/model/IExtentedGridInfoService.java
@@ -1,18 +1,8 @@
 package eu.geclipse.info.model;
 
-import java.net.URI;
-
-import org.eclipse.core.filesystem.IFileStore;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 
-import eu.geclipse.core.model.IGridComputing;
-import eu.geclipse.core.model.IGridContainer;
 import eu.geclipse.core.model.IGridInfoService;
-import eu.geclipse.core.model.IGridProject;
-import eu.geclipse.core.model.IGridService;
-import eu.geclipse.core.model.IGridStorage;
 import eu.geclipse.core.model.IVirtualOrganization;
 import eu.geclipse.info.IGlueInfoStore;
 
