9e9a29728df7302c2a58448e62743abcb713bdf7
Jie Tao <jie.tao@iwr.fzk.de>
2008-01-16 10:34:20 +0000

    add copyrights

--- plugins/eu.geclipse.jsdl.model/src/eu/geclipse/jsdl/model/util/JsdlResourceImpl.java
+++ plugins/eu.geclipse.jsdl.model/src/eu/geclipse/jsdl/model/util/JsdlResourceImpl.java
@@ -1,9 +1,19 @@
-/**
- * <copyright>
- * </copyright>
+/******************************************************************************
+ * Copyright (c) 2007 g-Eclipse consortium 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- * $Id: JsdlResourceImpl.java,v 1.2 2007/03/01 09:15:18 emstamou Exp $
- */
+ * Initial development of the original code was made for
+ * project g-Eclipse founded by European Union
+ * project number: FP6-IST-034327  http://www.geclipse.eu/
+ *
+ * Contributor(s):
+ *    Mathias Stümpert
+ *           
+ *****************************************************************************/
+
 package eu.geclipse.jsdl.model.util;
 
 import org.eclipse.emf.common.util.URI;
