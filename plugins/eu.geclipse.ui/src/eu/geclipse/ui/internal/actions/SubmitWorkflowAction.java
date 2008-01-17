4f721b9d283750070abe2dc7bfff5421fad4d715
Pawel Wolniewicz <pwolniewi>
2008-01-17 13:44:36 +0000

    reformatted

--- plugins/eu.geclipse.ui/src/eu/geclipse/ui/internal/actions/SubmitWorkflowAction.java
+++ plugins/eu.geclipse.ui/src/eu/geclipse/ui/internal/actions/SubmitWorkflowAction.java
@@ -25,50 +25,52 @@ import eu.geclipse.core.model.IGridWorkflow;
 
 public class SubmitWorkflowAction extends SelectionListenerAction {
 
-	/**
-	 * The workbench site this action belongs to.
-	 */
-	private IWorkbenchSite site;
+  /**
+   * The workbench site this action belongs to.
+   */
+  private IWorkbenchSite site;
+  private ArrayList<IGridWorkflow> workflow;
 
-	private ArrayList<IGridWorkflow> workflow;
+  protected SubmitWorkflowAction( final IWorkbenchSite site ) {
+    super( Messages.getString( "SubmitWorkflowAction.title" ) ); //$NON-NLS-1$
+    this.site = site;
+  }
 
-	protected SubmitWorkflowAction(final IWorkbenchSite site) {
-		super(Messages.getString("SubmitWorkflowAction.title")); //$NON-NLS-1$
-		this.site = site;
-	}
+  /*
+   * (non-Javadoc)
+   * 
+   * @see org.eclipse.jface.action.Action#run()
+   */
+  @Override
+  public void run() {
+    // JobCreatorSelectionWizard wizard = new JobCreatorSelectionWizard(
+    // this.workflow, this.workflow);
+    // WizardDialog dialog = new WizardDialog(this.site.getShell(), wizard);
+    // dialog.open();
+  }
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.jface.action.Action#run()
-	 */
-	@Override
-	public void run() {
-//		JobCreatorSelectionWizard wizard = new JobCreatorSelectionWizard(
-//				this.workflow, this.workflow);
-//		WizardDialog dialog = new WizardDialog(this.site.getShell(), wizard);
-//		dialog.open();
-	}
+  /*
+   * (non-Javadoc)
+   * 
+   * @see org.eclipse.ui.actions.BaseSelectionListenerAction#updateSelection(org.eclipse.jface.viewers.IStructuredSelection)
+   */
+  @Override
+  protected boolean updateSelection( final IStructuredSelection selection ) {
+    this.workflow = new ArrayList<IGridWorkflow>();
+    boolean enabled = super.updateSelection( selection );
+    Iterator<?> iter = selection.iterator();
+    while( iter.hasNext() && enabled ) {
+      Object element = iter.next();
+      boolean isWorkflowFile = isWorkflow( element );
+      enabled &= isWorkflowFile;
+      if( isWorkflowFile ) {
+        this.workflow.add( ( IGridWorkflow )element );
+      }
+    }
+    return enabled && !this.workflow.isEmpty();
+  }
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.ui.actions.BaseSelectionListenerAction#updateSelection(org.eclipse.jface.viewers.IStructuredSelection)
-	 */
-	@Override
-	protected boolean updateSelection(final IStructuredSelection selection) {
-		this.workflow = new ArrayList<IGridWorkflow>();
-		boolean enabled = super.updateSelection(selection);
-		Iterator<?> iter = selection.iterator();
-		while (iter.hasNext() && enabled) {
-			Object element = iter.next();
-			boolean isWorkflowFile = isWorkflow(element);
-			enabled &= isWorkflowFile;
-			if (isWorkflowFile) {
-				this.workflow.add((IGridWorkflow) element);
-			}
-		}
-		return enabled && !this.workflow.isEmpty();
-	}
-
-	protected boolean isWorkflow(final Object element) {
-		return element instanceof eu.geclipse.core.model.IGridWorkflow;
-	}
-
+  protected boolean isWorkflow( final Object element ) {
+    return element instanceof eu.geclipse.core.model.IGridWorkflow;
+  }
 }
