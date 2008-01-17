66b15de551a33833ede9f2f882995d1af724a47e
Katarzyna Bylec <katarzyna.bylec@gmail.com>
2008-01-17 09:02:27 +0000

    Initial import.

---
+++ test/eu.geclipse.jsdl.ui.test/src/eu/geclipse/jsdl/ui/adapters/jsdl/JobDefinitionTypeAdapter_Test.java
@@ -0,0 +1,60 @@
+/******************************************************************************
+ * Copyright (c) 2007 g-Eclipse consortium 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Initial development of the original code was made for
+ * project g-Eclipse founded by European Union
+ * project number: FP6-IST-034327  http://www.geclipse.eu/
+ *
+ * Contributor(s):
+ *     PSNC: 
+ *      - Katarzyna Bylec (katis@man.poznan.pl)
+ *           
+ *****************************************************************************/
+package eu.geclipse.jsdl.ui.adapters.jsdl;
+
+import org.junit.Assert;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+public class JobDefinitionTypeAdapter_Test {
+
+  @BeforeClass
+  public static void setUpBeforeClass() throws Exception {
+  }
+
+  @Test
+  public void testJobDefinitionTypeAdapter() {
+    fail( "Not yet implemented" ); // TODO
+  }
+
+  @Test
+  public void testContentChanged() {
+    fail( "Not yet implemented" ); // TODO
+  }
+
+  @Test
+  public void testAttachID() {
+    fail( "Not yet implemented" ); // TODO
+  }
+
+  @Test
+  public void testSetContent() {
+    fail( "Not yet implemented" ); // TODO
+  }
+
+  @Test
+  public void testLoad() {
+    fail( "Not yet implemented" ); // TODO
+  }
+
+  @Test
+  public void testIsEmpty() {
+    fail( "Not yet implemented" ); // TODO
+  }
+}
