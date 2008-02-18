c1d4cf38e553e1ef435b656cd8ba90af7b6ab6bc
Ariel Garcia <agarcia>
2008-02-18 17:57:14 +0000

    Update to new problem reporting: remove tests of old problemProvider extension point

--- test/eu.geclipse.core.test/src/eu/geclipse/core/Extensions_PDETest.java
+++ test/eu.geclipse.core.test/src/eu/geclipse/core/Extensions_PDETest.java
@@ -1,5 +1,5 @@
 /*****************************************************************************
- * Copyright (c) 2006, 2007 g-Eclipse Consortium 
+ * Copyright (c) 2006-2008 g-Eclipse Consortium 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -85,16 +85,4 @@ public class Extensions_PDETest {
     Assert.assertEquals( new Integer( 8 ),new Integer( elementcreators.size() ));
    }
 
-  /** test method {@link Extensions#getRegisteredProblemProviders()}
-   * 
-   *
-   */
-  
-  @Test
-  public void testGetRegisteredProblemProviders()
-  {
-    List< IProblemProvider > problemproviders;
-    problemproviders = Extensions.getRegisteredProblemProviders();
-    Assert.assertEquals( new Integer( 9 ),new Integer( problemproviders.size() ));
-  }
 }
