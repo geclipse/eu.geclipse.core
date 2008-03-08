a0771fae06cd6aa5c24515509316453940553cb0
Ariel Garcia <agarcia>
2008-03-08 12:39:26 +0000

    Cleanup some warnings

--- plugins/eu.geclipse.webview/src/eu/geclipse/webview/views/Messages.java
+++ plugins/eu.geclipse.webview/src/eu/geclipse/webview/views/Messages.java
@@ -22,20 +22,20 @@ import org.eclipse.osgi.util.NLS;
  */
 public class Messages extends NLS {
 
-  public static final String WebView_project_home = "Project Home";
-  
   public static String WebView_action_2_tooltip;
 
   public static String WebView_back;
   public static String WebView_forward;
 
+  public static String WebView_initializeError;
+
+  public static String WebView_project_home;
   public static String WebView_g_eclipse;
   public static String WebView_GGUS;
   public static String WebView_GGUSshow;
-  public static String WebView_initializeError;
   public static String WebView_VOMS;
   public static String WebView_VOMSshow;
-
+  
   private static final String BUNDLE_NAME = "eu.geclipse.webview.views.messages"; //$NON-NLS-1$
   
   static {
