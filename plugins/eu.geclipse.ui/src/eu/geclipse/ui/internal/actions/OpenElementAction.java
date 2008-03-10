5175748eefa83d318c5010e2ffd0f3cc74611f68
Mariusz Wojtysiak <mwojtysia>
2008-03-10 08:41:48 +0000

    218473 'Permission denied' is not handled properly during gridFTP operations

--- plugins/eu.geclipse.ui/src/eu/geclipse/ui/internal/actions/OpenElementAction.java
+++ plugins/eu.geclipse.ui/src/eu/geclipse/ui/internal/actions/OpenElementAction.java
@@ -21,12 +21,18 @@ import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.actions.BaseSelectionListenerAction;
 import org.eclipse.ui.actions.OpenFileAction;
 
 import eu.geclipse.core.model.IGridConnectionElement;
 import eu.geclipse.core.model.IGridJob;
+import eu.geclipse.core.reporting.ProblemException;
+import eu.geclipse.ui.dialogs.ProblemDialog;
 
 /**
  * Action for opening Grid model elements.
@@ -70,8 +76,26 @@ public class OpenElementAction
    */
   @Override
   public void run() {
-    if ( this.activeAction != null ) {
-      this.activeAction.run();
+    if( this.activeAction != null ) {
+      try {
+        this.activeAction.run();
+      } catch( RuntimeException exc ) {
+        if( exc.getCause() instanceof ProblemException ) {
+          ProblemException problemExc = ( ProblemException )exc.getCause();
+          Shell shell = null;
+          IWorkbench workbench = PlatformUI.getWorkbench();
+          IWorkbenchWindow window = workbench.getActiveWorkbenchWindow();
+          if( window != null ) {
+            shell = window.getShell();
+          }
+          ProblemDialog.openProblem( shell,
+                                     Messages.getString("OpenElementAction.problemDialogTitle"), //$NON-NLS-1$
+                                     Messages.getString("OpenElementAction.problemCannotOpen"), //$NON-NLS-1$
+                                     problemExc );
+        } else {
+          throw exc;
+        }
+      }
     }
   }
   
