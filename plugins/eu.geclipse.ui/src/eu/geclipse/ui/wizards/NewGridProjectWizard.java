49d9c0d908c564143d9b377d15dd62af536f96b7
Pawel Wolniewicz <pwolniewi>
2008-03-11 14:56:49 +0000

    Switching to GridUserPerspective when GridProjectWizard finishes.

--- plugins/eu.geclipse.ui/src/eu/geclipse/ui/wizards/NewGridProjectWizard.java
+++ plugins/eu.geclipse.ui/src/eu/geclipse/ui/wizards/NewGridProjectWizard.java
@@ -12,6 +12,7 @@
  * Contributors:
  *    Mathias Stuempert - initial API and implementation
  *    Harald Kornmayer 
+ *    Pawel Wolniewicz
  *****************************************************************************/
 
 package eu.geclipse.ui.wizards;
@@ -20,15 +21,31 @@ import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.dialogs.ErrorDialog;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.dialogs.MessageDialogWithToggle;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.preference.PreferenceStore;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.IWizardPage;
 import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IPerspectiveDescriptor;
+import org.eclipse.ui.IPerspectiveRegistry;
 import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.actions.WorkspaceModifyDelegatingOperation;
 import org.eclipse.ui.dialogs.WizardNewProjectCreationPage;
 import org.eclipse.ui.dialogs.WizardNewProjectReferencePage;
+import org.eclipse.ui.internal.WorkbenchPlugin;
+import org.eclipse.ui.internal.registry.PerspectiveDescriptor;
+
 import eu.geclipse.ui.internal.Activator;
 
 /**
@@ -55,7 +72,71 @@ public class NewGridProjectWizard extends Wizard implements INewWizard {
   @Override
   public boolean performFinish() {
     IProject proj = createNewProject();
+    if(proj!=null){
+      switchPerspective();
+    }
     return proj != null;
+  }
+
+  private void switchPerspective() {
+    final IWorkbench workBench = Activator.getDefault().getWorkbench();
+    Display display = workBench.getDisplay();
+    display.asyncExec( new Runnable() {
+
+      public void run() {
+        IWorkbenchPage page = null;
+        IWorkbenchWindow window = workBench.getActiveWorkbenchWindow();
+        if( window != null ) {
+          page = window.getActivePage();
+          if( page != null ) {
+            IPerspectiveDescriptor perspectiveDescriptor = page.getPerspective();
+            if( perspectiveDescriptor != null
+                && !perspectiveDescriptor.getId()
+                  .equals( Activator.ID_USER_PERSPECTIVE ) )
+            {
+              try {
+                String newLine = System.getProperty( "line.separator" ); //$NON-NLS-1$
+                String message = Messages.getString("NewGridProjectWizard.PerspectiveSwitchingInfo_line1"); //$NON-NLS-1$
+                message += newLine + newLine;
+                message += Messages.getString("NewGridProjectWizard.PerspectiveSwitchingInfo_line2"); //$NON-NLS-1$
+                message += newLine + newLine;
+                message += Messages.getString("NewGridProjectWizard.PerspectiveSwitchingInfo_line3"); //$NON-NLS-1$
+                String toggleMessage = Messages.getString("NewGridProjectWizard.RememberQuestion"); //$NON-NLS-1$
+                String key = "RememberSwitchingPerspective"; //$NON-NLS-1$
+                IPreferenceStore ps = Activator.getDefault()
+                  .getPreferenceStore();
+                String ask=ps.getString( key );
+                if(MessageDialogWithToggle.NEVER.equalsIgnoreCase( ask )){
+                  return;
+                }
+                if(MessageDialogWithToggle.ALWAYS.equalsIgnoreCase( ask )
+                    || MessageDialogWithToggle.openYesNoQuestion( window.getShell(),
+                                                                  Messages.getString("NewGridProjectWizard.ConfirmPerpectiveSwitch"), //$NON-NLS-1$
+                                                                  message,
+                                                                  toggleMessage,
+                                                                  false,
+                                                                  ps,
+                                                                  key )
+                      .getReturnCode() == IDialogConstants.YES_ID )
+                {
+                  window.getWorkbench()
+                    .showPerspective( Activator.ID_USER_PERSPECTIVE, window );
+                }
+              } catch( Exception e ) {
+                Status status = new Status( Status.ERROR,
+                                            Activator.PLUGIN_ID,
+                                            Messages.getString("NewGridProjectWizard.PerspectiveSwitchingError"), //$NON-NLS-1$
+                                            e );
+                ErrorDialog.openError( window.getShell(),
+                                       Messages.getString("NewGridProjectWizard.PerspectiveSwitchingError"), //$NON-NLS-1$
+                                       Messages.getString("NewGridProjectWizard.ManualPerspectiveSwitching"), //$NON-NLS-1$
+                                       status );
+              }
+            }
+          }
+        }
+      }
+    } );
   }
 
   /**
