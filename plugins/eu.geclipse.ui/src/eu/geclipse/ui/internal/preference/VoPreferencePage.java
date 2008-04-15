df58280396c13138d34782fddf16315491fad524
Thomas Köckerbauer <tkoeck@gup.jku.at>
2008-04-15 15:07:49 +0000

    removed template comment

--- plugins/eu.geclipse.ui/src/eu/geclipse/ui/internal/preference/VoPreferencePage.java
+++ plugins/eu.geclipse.ui/src/eu/geclipse/ui/internal/preference/VoPreferencePage.java
@@ -74,17 +74,6 @@ import eu.geclipse.ui.internal.wizards.VoImportWizard;
 import eu.geclipse.ui.listeners.TableColumnListener;
 import eu.geclipse.ui.wizards.wizardselection.ExtPointWizardSelectionListPage;
 
-
-/**
- * This class represents a preference page that is contributed to the
- * PreferenceConstants dialog. By subclassing <samp>FieldEditorPreferencePage</samp>,
- * we can use the field support built into JFace that allows us to create a page
- * that is small and knows how to save, restore and apply itself.
- * <p>
- * This page is used to modify preferences only. They are stored in the
- * preference store that belongs to the main plug-in class. That way,
- * preferences can be accessed directly via the preference store.
- */
 public class VoPreferencePage
     extends PreferencePage
     implements IWorkbenchPreferencePage, IGridModelListener {
