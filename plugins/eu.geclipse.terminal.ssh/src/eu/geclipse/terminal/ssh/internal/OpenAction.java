a9ba00950b9c9b041f6c3a2b9837e06000cd54e6
Ariel Garcia <agarcia>
2008-04-25 12:00:47 +0000

    Fix copyrights

--- plugins/eu.geclipse.terminal.ssh/src/eu/geclipse/terminal/ssh/internal/OpenAction.java
+++ plugins/eu.geclipse.terminal.ssh/src/eu/geclipse/terminal/ssh/internal/OpenAction.java
@@ -1,3 +1,18 @@
+/*****************************************************************************
+ * Copyright (c) 2008 g-Eclipse Consortium 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Initial development of the original code was made for the
+ * g-Eclipse project founded by European Union
+ * project number: FP6-IST-034327  http://www.geclipse.eu/
+ *
+ * Contributors:
+ *    Thomas Koeckerbauer GUP, JKU - initial API and implementation
+ *****************************************************************************/
+
 package eu.geclipse.terminal.ssh.internal;
 
 import org.eclipse.jface.action.IAction;
