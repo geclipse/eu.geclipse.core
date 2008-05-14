fcb28eb2ac80417d7590321d0ccdf0f05a2e15d6
Harald Gjermundrod <hgjermund>
2008-05-14 08:15:29 +0000

    Added the missing copyright

--- plugins/eu.geclipse.info/src/eu/geclipse/info/model/GridApplication.java
+++ plugins/eu.geclipse.info/src/eu/geclipse/info/model/GridApplication.java
@@ -1,3 +1,19 @@
+/******************************************************************************
+ * Copyright (c) 2008 g-Eclipse consortium
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Initial development of the original code was made for
+ * project g-Eclipse founded by European Union
+ * project number: FP6-IST-034327  http://www.geclipse.eu/
+ *
+ * Contributor(s):
+ *     UCY (http://www.ucy.cs.ac.cy)
+ *      - Nikolaos Tsioutsias
+ *
+ *****************************************************************************/
 package eu.geclipse.info.model;
 
 import java.net.URI;
