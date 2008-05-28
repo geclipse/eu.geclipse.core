d783142e48e8f5fac1f069e2935aa7e73fa1febd
Thomas Köckerbauer <tkoeck@gup.jku.at>
2008-05-28 13:52:38 +0000

    cleanup of the wizard UI, made the UI more Eclipse style compliant

--- plugins/eu.geclipse.batch.ui/src/eu/geclipse/batch/ui/wizards/AddQueueWizardRequiredPage.java
+++ plugins/eu.geclipse.batch.ui/src/eu/geclipse/batch/ui/wizards/AddQueueWizardRequiredPage.java
@@ -33,6 +33,7 @@ import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Spinner;
 import org.eclipse.swt.widgets.Table;
@@ -50,54 +51,54 @@ import eu.geclipse.batch.ui.internal.Messages;
 import eu.geclipse.ui.dialogs.ProblemDialog;
 
 /**
- * Required page for the create Queue wizard 
+ * Required page for the create Queue wizard
  */
 public class AddQueueWizardRequiredPage extends WizardPage {
 
   // Controls
-  
+
   // Protected
   protected Text nameText;
-  
+
   protected Combo typeCombo;
 
   protected Button enabledButton;  //radio button
-  
+
   protected Button disabledButton; //radio button
-  
+
   protected Spinner timeCPUHourSpin;
-  
+
   protected Spinner timeCPUMinSpin;
-  
+
   protected Spinner timeWallHourSpin;
-  
+
   protected Spinner timeWallMinSpin;
-  
+
   protected Table allowedVoTable;
-  
+
   // Private
-  private Composite mainComp; 
-  
+  private Composite mainComp;
+
   private Label nameLabel;
-    
+
   private Label typeLabel;
-  
+
   private Label statusLabel;
-  
+
   private Label timeCPULabel;
-  
+
   private Label timeWallLabel;
-  
-  private Composite statusComp;
+
+  private Group statusGroup;
 
   private Composite tableButtonsComp;
-  
+
   private Button addVoButton;
-  
+
   private Button editVoButton;
-  
+
   private Button deleteVoButton;
-  
+
   /**
    * Create a new instance of the AddQueueWizardRequiredPage
    */
@@ -106,27 +107,26 @@ public class AddQueueWizardRequiredPage extends WizardPage {
     this.setTitle( Messages.getString( "AddQueueRequiredPage.Title" ) ); //$NON-NLS-1$
     this.setDescription( Messages.getString( "AddQueueRequiredPage.Description" ) ); //$NON-NLS-1$
   }
-  
+
   /**
    * Create the controls to be displayed n the wizard.
    * @param parent The parent widget of the page.
    */
   public void createControl( final Composite parent ) {
-    
+
     GridData longData, statusData, voButtonData;
     Composite timeComp;
-    
+
     this.mainComp = new Composite( parent, SWT.NONE );
     this.mainComp.setLayout( new GridLayout( 4, false ) );
-    
+
     // Queue Name text box
-    longData = new GridData( GridData.HORIZONTAL_ALIGN_FILL );
-    longData.grabExcessHorizontalSpace = true;
+    longData = new GridData( SWT.FILL, SWT.FILL, true, false );
     longData.horizontalSpan = 3;
-    
+
     this.nameLabel = new Label( this.mainComp, SWT.NONE );
     this.nameLabel.setText( Messages.getString( "AddQueueRequiredPage.Name" ) ); //$NON-NLS-1$
-    
+
     this.nameText = new Text( this.mainComp, SWT.BORDER );
     this.nameText.setLayoutData( longData );
 
@@ -135,94 +135,103 @@ public class AddQueueWizardRequiredPage extends WizardPage {
         updateUI();
       }
     } );
-    
+
     // Queue Type ComboBox
     this.typeLabel = new Label( this.mainComp, SWT.NONE );
     this.typeLabel.setText( Messages.getString( "AddQueueRequiredPage.Type" ) ); //$NON-NLS-1$
-    
+
     this.typeCombo = new Combo( this.mainComp, SWT.DROP_DOWN );
     this.typeCombo.add( Messages.getString( "AddQueueRequiredPage.Type_Execution" ), 0 ); //$NON-NLS-1$
     this.typeCombo.add( Messages.getString( "AddQueueRequiredPage.Type_Route" ), 1 ); //$NON-NLS-1$
     this.typeCombo.select( 0 );
-    
-    // Status enable and disable radio buttons
-    this.statusLabel = new Label( this.mainComp, SWT.NONE );
-    this.statusLabel.setText( Messages.getString( "AddQueueRequiredPage.Status" ) ); //$NON-NLS-1$
-    
-    statusData = new GridData( GridData.GRAB_HORIZONTAL );
-    statusData.verticalSpan = 2;
-    
-    this.statusComp = new Composite( this.mainComp, SWT.NONE );
-    this.statusComp.setLayoutData( statusData );
-    this.statusComp.setLayout( new GridLayout( 1, false ) );
+    this.typeCombo.setLayoutData( longData );
 
-    this.enabledButton = new Button( this.statusComp, SWT.RADIO );
+    // Status enable and disable radio buttons
+
+    this.statusGroup = new Group( this.mainComp, SWT.NONE );
+    this.statusGroup.setText(  Messages.getString( "AddQueueRequiredPage.Status" )  ); //$NON-NLS-1$
+    this.statusGroup.setLayout( new GridLayout( 1, false ) );
+    this.statusGroup.setLayoutData( new GridData( SWT.FILL, SWT.FILL, true, false, 4, 1 ) );
+
+    this.enabledButton = new Button( this.statusGroup, SWT.RADIO );
     this.enabledButton.setText( Messages.getString( "AddQueueRequiredPage.Enabled" ) ); //$NON-NLS-1$
     this.enabledButton.setSelection( true );
-    
-    this.disabledButton = new Button( this.statusComp, SWT.RADIO );
+
+    this.disabledButton = new Button( this.statusGroup, SWT.RADIO );
     this.disabledButton.setText( Messages.getString( "AddQueueRequiredPage.Disabled" ) ); //$NON-NLS-1$
-    
-    new Label(this.mainComp, SWT.NONE).setLayoutData( longData ); // Empty Label
-    
+
+//    new Label(this.mainComp, SWT.NONE).setLayoutData( longData ); // Empty Label
+
     // Maximum Time CPU spinner
     this.timeCPULabel = new Label( this.mainComp, SWT.NONE );
     this.timeCPULabel.setText( Messages.getString( "AddQueueRequiredPage.TimeCPU" ) ); //$NON-NLS-1$
-    
+
     timeComp = new Composite( this.mainComp, SWT.NONE );
     timeComp.setLayout( new GridLayout( 3, false ) );
-    
-    this.timeCPUHourSpin = new Spinner( timeComp, SWT.NONE );
+
+    this.timeCPUHourSpin = new Spinner( timeComp, SWT.BORDER );
 //    this.timeCPUHourSpin.setValues( 48, 0, 999, 0, 1, 2 );
     this.timeCPUHourSpin.setValues( 172800, 0, 9999999, 0, 1000, 2000 );
-    
+
 //    new Label( timeComp, SWT.NONE ).setText( ":" ); //$NON-NLS-1$
-//    
+//
 //    this.timeCPUMinSpin = new Spinner( timeComp, SWT.NONE );
 //    this.timeCPUMinSpin.setValues( 0, 0, 59, 0, 1, 2 );
-   
+
     // Maximum Time wall spinner
     this.timeWallLabel = new Label( this.mainComp, SWT.NONE );
     this.timeWallLabel.setText( Messages.getString( "AddQueueRequiredPage.TimeWall" ) ); //$NON-NLS-1$
 
     timeComp = new Composite( this.mainComp, SWT.NONE );
     timeComp.setLayout( new GridLayout( 3, false ) );
-    
-    this.timeWallHourSpin = new Spinner( timeComp, SWT.NONE );
+
+    this.timeWallHourSpin = new Spinner( timeComp, SWT.BORDER );
     this.timeWallHourSpin.setValues( 172800, 0, 9999999, 0, 1000, 2000 );
 //    this.timeWallHourSpin.setValues( 72, 0, 999, 0, 1, 2 );
-    
+
 //    new Label( timeComp, SWT.NONE ).setText( ":" ); //$NON-NLS-1$
-//    
+//
 //    this.timeWallMinSpin = new Spinner( timeComp, SWT.NONE );
 //    this.timeWallMinSpin.setValues( 0, 0, 59, 0, 1, 2 );
-    
+
+    GridLayout compLayout = new GridLayout(2, false);
+    compLayout.marginHeight = 0;
+    compLayout.marginWidth = 0;
+
     Composite tableComp = new Composite( this.mainComp, SWT.NONE );
     // Create the table that holds the result
     this.allowedVoTable = new Table( tableComp, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL | SWT.BORDER );
     this.allowedVoTable.setHeaderVisible( true );
-    this.allowedVoTable.setLinesVisible( true );    
+    this.allowedVoTable.setLinesVisible( true );
+    this.allowedVoTable.addSelectionListener( new SelectionAdapter() {
+      @SuppressWarnings("unqualified-field-access")
+      @Override
+      public void widgetSelected( final SelectionEvent e ) {
+        editVoButton.setEnabled( true );
+        deleteVoButton.setEnabled( true );
+      }
+    } );
 
     GridData gd = new GridData( SWT.FILL, SWT.FILL, true, true );
     this.allowedVoTable.setLayoutData( gd );
 
-    GridData gridData = new GridData( SWT.BEGINNING, SWT.BEGINNING, true, true );
-    gridData.widthHint = 300;
-    gridData.heightHint = 150;
-    tableComp.setLayout( new GridLayout( 1, false ) );
-    tableComp.setLayoutData( gridData );
+    tableComp.setLayout( compLayout );
+    tableComp.setLayoutData( new GridData( SWT.FILL, SWT.FILL, true, true, 4, 1 ) );
 
     TableColumn hostColumn = new TableColumn( this.allowedVoTable, SWT.LEFT );
     hostColumn.setText( Messages.getString( "AddQueueRequiredPage.AllowedVos" ) );//$NON-NLS-1$
     hostColumn.setWidth( 250 );
-    
-    this.tableButtonsComp = new Composite( this.mainComp, SWT.NONE );
-    this.tableButtonsComp.setLayout( new GridLayout() );
-    this.tableButtonsComp.setLayoutData( new GridData( SWT.LEFT, SWT.TOP, true, false ) );
-    
-    voButtonData = new GridData();
-    voButtonData.grabExcessHorizontalSpace = true;
-    
+
+    GridLayout buttonCompLayout = new GridLayout();
+    buttonCompLayout.marginHeight = 0;
+    buttonCompLayout.marginWidth = 0;
+
+    this.tableButtonsComp = new Composite( tableComp, SWT.NONE );
+    this.tableButtonsComp.setLayout( buttonCompLayout );
+    this.tableButtonsComp.setLayoutData( new GridData( SWT.FILL, SWT.FILL, false, false ) );
+
+    voButtonData = new GridData(SWT.FILL, SWT.FILL, true, false);
+
     this.addVoButton = new Button( this.tableButtonsComp, SWT.PUSH );
     this.addVoButton.setText( Messages.getString( "AddQueueRequiredPage.AddVo" ) ); //$NON-NLS-1$
     this.addVoButton.setLayoutData( voButtonData );
@@ -230,7 +239,7 @@ public class AddQueueWizardRequiredPage extends WizardPage {
       @SuppressWarnings("unqualified-field-access")
       @Override
       public void widgetSelected (final SelectionEvent e ){
-        AllowedVOsDialog dialog = 
+        AllowedVOsDialog dialog =
           new AllowedVOsDialog( getShell(),
                                 Messages.getString( "AddQueueRequiredPage.AddNewVOTitle" ) ); //$NON-NLS-1$
         if ( dialog.open() == Window.OK ) {
@@ -239,17 +248,18 @@ public class AddQueueWizardRequiredPage extends WizardPage {
         }
       }
     });
-    
+
     this.editVoButton = new Button( this.tableButtonsComp, SWT.PUSH );
     this.editVoButton.setText( Messages.getString( "AddQueueRequiredPage.EditVo" ) ); //$NON-NLS-1$
     this.editVoButton.setLayoutData( voButtonData );
+    this.editVoButton.setEnabled( false );
     this.editVoButton.addSelectionListener( new SelectionAdapter(){
       @SuppressWarnings("unqualified-field-access")
       @Override
       public void widgetSelected( final SelectionEvent e ){
         TableItem [] selectedItems = allowedVoTable.getSelection();
         if ( selectedItems.length == 1 ){
-          AllowedVOsDialog dialog = 
+          AllowedVOsDialog dialog =
             new AllowedVOsDialog( getShell(),
                                   Messages.getString( "AddQueueRequiredPage.AddNewVOTitle" ) ); //$NON-NLS-1$
 
@@ -260,28 +270,29 @@ public class AddQueueWizardRequiredPage extends WizardPage {
         }
       }
     });
-    
+
     this.deleteVoButton = new Button( this.tableButtonsComp, SWT.PUSH );
     this.deleteVoButton.setText( Messages.getString( "AddQueueRequiredPage.DeleteVo" ) ); //$NON-NLS-1$
     this.deleteVoButton.setLayoutData( voButtonData );
+    this.deleteVoButton.setEnabled( false );
     this.deleteVoButton.addSelectionListener( new SelectionAdapter(){
       @SuppressWarnings("unqualified-field-access")
       @Override
       public void widgetSelected( final SelectionEvent e ){
         int [] selectedItems = allowedVoTable.getSelectionIndices();
         if ( selectedItems.length > 0 ) {
-          allowedVoTable.remove( selectedItems );       
+          allowedVoTable.remove( selectedItems );
         }
       }
     });
-    
+
     setControl( this.mainComp );
   }
-  
+
   protected void updateUI() {
     setPageComplete( isInputValid() );
   }
-  
+
   /* (non-Javadoc)
    * @see org.eclipse.jface.wizard.WizardPage#isPageComplete()
    */
@@ -310,43 +321,43 @@ public class AddQueueWizardRequiredPage extends WizardPage {
 
   protected List<String> getVONames(){
     ArrayList<String> voList = null;
-    
+
     if ( 0 != this.allowedVoTable.getItemCount() ) {
       voList = new ArrayList<String>();
       for( TableItem item:this.allowedVoTable.getItems() )
         voList.add( item.getText() );
     }
-    
+
     return voList;
   }
-  
+
 //  protected String getTimeCPU() {
 //    return String.format( "%02d:%02d:00", //$NON-NLS-1$
-//                          new Integer( this.timeCPUHourSpin.getSelection() ), 
-//                          new Integer( this.timeCPUMinSpin.getSelection() ) ); 
+//                          new Integer( this.timeCPUHourSpin.getSelection() ),
+//                          new Integer( this.timeCPUMinSpin.getSelection() ) );
 //  }
 //
 //  protected String getTimeWall() {
 //    return String.format( "%02d:%02d:00", //$NON-NLS-1$
-//                          new Integer( this.timeWallHourSpin.getSelection() ), 
-//                          new Integer( this.timeWallMinSpin.getSelection() ) ); 
+//                          new Integer( this.timeWallHourSpin.getSelection() ),
+//                          new Integer( this.timeWallMinSpin.getSelection() ) );
 //  }
-  
-  
+
+
   protected int getTimeCPU() {
-    return this.timeCPUHourSpin.getSelection(); 
+    return this.timeCPUHourSpin.getSelection();
   }
 
   protected int getTimeWall() {
-    return this.timeWallHourSpin.getSelection(); 
+    return this.timeWallHourSpin.getSelection();
   }
- 
-  
+
+
   /**
-   * Creates a queue if user have the rights, if not an error dialog is displayed 
-   * @param qdlFile 
+   * Creates a queue if user have the rights, if not an error dialog is displayed
+   * @param qdlFile
    * @return ret
-   * 
+   *
    */
   protected boolean finish(final IFile qdlFile) {
 
@@ -355,8 +366,8 @@ public class AddQueueWizardRequiredPage extends WizardPage {
     BatchQueueDescription batchQueueDescription = new BatchQueueDescription(qdlFile);
     try {
       batchQueueDescription.setQueueName( this.nameText.getText().trim());
-          
-      str = this.typeCombo.getText(); 
+
+      str = this.typeCombo.getText();
       if ( str.equals( Messages.getString( "AddQueueRequiredPage.Type_Execution" ) ) ) //$NON-NLS-1$
         batchQueueDescription.queueType( QueueTypeEnumeration.EXECUTION );
       else
@@ -368,30 +379,30 @@ public class AddQueueWizardRequiredPage extends WizardPage {
       else {
         batchQueueDescription.enableQueue( false );
       }
-        
+
       /* Set Max CPU Time Limit */
       RangeValueType rangeValueType = QdlFactory.eINSTANCE.createRangeValueType();
       BoundaryType boundaryType = QdlFactory.eINSTANCE.createBoundaryType();
       boundaryType.setValue( getTimeCPU() );
-      rangeValueType.setUpperBoundedRange( boundaryType );        
-        
+      rangeValueType.setUpperBoundedRange( boundaryType );
+
       batchQueueDescription.getRoot().getQueue().setCPUTimeLimit( rangeValueType );
-                
+
       /* Set Max Time Wall Limit */
       rangeValueType = QdlFactory.eINSTANCE.createRangeValueType();
       boundaryType = QdlFactory.eINSTANCE.createBoundaryType();
       boundaryType.setValue( getTimeWall() );
-        
-      rangeValueType.setUpperBoundedRange( boundaryType );              
+
+      rangeValueType.setUpperBoundedRange( boundaryType );
       batchQueueDescription.getRoot().getQueue().setWallTimeLimit( rangeValueType );
-        
+
       if (this.allowedVoTable.getItemCount() > 0 ) {
         Collection <String> allowedVoList = new ArrayList<String>();
         for(int i=0; i < this.allowedVoTable.getItemCount(); i++) {
           allowedVoList.add( this.allowedVoTable.getItem( i ).getText() );
         }
         batchQueueDescription.setAllowedVirtualOrganizations( allowedVoList );
-          
+
       }
       batchQueueDescription.save(qdlFile);
       ret = true;
@@ -400,7 +411,7 @@ public class AddQueueWizardRequiredPage extends WizardPage {
       ProblemDialog.openProblem( this.getShell(),
                                     Messages.getString( "AddQueueWizard.error_manipulate_title" ),  //$NON-NLS-1$
                                     Messages.getString( "AddQueueWizard.error_manipulate_message" ), //$NON-NLS-1$
-                                    excp );      
+                                    excp );
     }
     return ret;
   }
