e15d887760461dc6d7ab6247574d590ca9b7ba6b
Nicholas Loulloudes <loulloudes.n@cs.ucy.ac.cy>
2008-05-29 14:34:11 +0000

    This commit add's the DoubleNumberVerifier SWT listener. This listener can be used to check whether the input of an SWT Text is a Double. It allows only positive double numbers

---
+++ plugins/eu.geclipse.ui/src/eu/geclipse/ui/widgets/DoubleNumberVerifier.java
@@ -0,0 +1,48 @@
+/******************************************************************************
+ * Copyright (c) 2008 g-Eclipse consortium
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Initial development of the original code was made for
+ * project g-Eclipse founded by European Union
+ * project number: FP6-IST-034327  http://www.geclipse.eu/
+ *
+ * Contributor(s):
+ *     UCY (http://www.ucy.cs.ac.cy)
+ *      - Nicholas Loulloudes (loulloudes.n@cs.ucy.ac.cy)
+ *
+ *****************************************************************************/
+package eu.geclipse.ui.widgets;
+
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
+
+
+/**
+ * Verifier for Text widgets. Allows to only enter (positive) float numbers. Usage:
+ * textWidget.addListener( SWT.Verify, new NumberVerifier() );
+ */
+public class DoubleNumberVerifier implements Listener {
+
+  private int counter = 0;
+  /* (non-Javadoc)
+   * @see org.eclipse.swt.widgets.Listener#handleEvent(org.eclipse.swt.widgets.Event)
+   */
+  public void handleEvent( final Event event ) {
+    String eventText = event.text;
+    
+    for( int ii = 0; ii < eventText.length(); ++ii ) {
+      if ( eventText.charAt( ii ) == '.' ) {
+        this.counter++;
+      }
+      
+      if( (!Character.isDigit( eventText.charAt( ii ) ) ) && ( eventText.charAt( ii ) != '.' ) || (this.counter > 1)) {
+        event.doit = false;
+        return;
+      }
+    }
+  
+  }
+}
