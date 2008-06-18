3fb8bd5ae3f2c1fc6854588b2555da080a5ae069
Mathias Stümpert <mathias.stuempert@iwr.fzk.de>
2008-06-18 13:52:52 +0000

    added convenience method for fetching resources

--- plugins/eu.geclipse.core/src/eu/geclipse/core/model/IGridInfoService.java
+++ plugins/eu.geclipse.core/src/eu/geclipse/core/model/IGridInfoService.java
@@ -23,6 +23,58 @@ import org.eclipse.core.runtime.IProgressMonitor;
  */
 public interface IGridInfoService extends IGridService {
   
+  /**
+   * Convenience method that fetches all resources for the specified VO
+   * and for the specified (inclusive) category without applying a type
+   * filter. So this is equivalent to
+   * <p>
+   * <code>fetchResources( parent, vo, category, false, null, monitor )</code>
+   * 
+   * @param parent The parent of the returned resources. This is the point in the
+   * Grid model were the resources are hooked in.
+   * @param vo The VO for which to query the information service. Only resources
+   * that are accessible for the specified VO will be returned.
+   * @param category The category of resources that should be fetched. This may be
+   * <code>null</code>. In that case either all (if <code>exclusive</code> is false)
+   * or no resources (if <code>exclusive</code> is true) will be returned.
+   * @param monitor A progress monitor used to monitor this operation.
+   * @return An array of Grid resources that apply to the specified arguments.
+   * @see #fetchResources(IGridContainer, IVirtualOrganization, IGridResourceCategory, boolean, Class, IProgressMonitor)
+   * @see IGridResourceCategory
+   */
+  public IGridResource[] fetchResources( final IGridContainer parent,
+                                         final IVirtualOrganization vo,
+                                         final IGridResourceCategory category,
+                                         final IProgressMonitor monitor );
+  
+  /**
+   * This method fetches resources that are available for the specified VO.
+   * The returned resources will belong to the specified resource category.
+   * If exclusive is <code>true</code> only resources of the specified category
+   * are returned, otherwise also resources of child categories are returned.
+   * If a <code>typeFilter</code> is defined the returned array will only contain
+   * elements of the specified type. This is a convenience functionality in order
+   * to prevent invokers from stepping through the array by themselves and checking
+   * each element against a specific type. 
+   * 
+   * @param parent The parent of the returned resources. This is the point in the
+   * Grid model were the resources are hooked in.
+   * @param vo The VO for which to query the information service. Only resources
+   * that are accessible for the specified VO will be returned.
+   * @param category The category of resources that should be fetched. This may be
+   * <code>null</code>. In that case either all (if <code>exclusive</code> is false)
+   * or no resources (if <code>exclusive</code> is true) will be returned.
+   * @param exclusive Determines if only resources specific for the specified
+   * resource category will be returned (<code>exclusive == true</code>) or if also
+   * resources of child categories will be returned (<code>exclusive == false</code>).
+   * @param typeFilter A filter that is applied to the returned resources. If such
+   * a filter is specified callers may savely cast the elements of the result array
+   * to the filtered type. This parameter may be <code>null</code>.
+   * @param monitor A progress monitor used to monitor this operation.
+   * @return An array of Grid resources that apply to the specified arguments.
+   * @see #fetchResources(IGridContainer, IVirtualOrganization, IGridResourceCategory, IProgressMonitor)
+   * @see IGridResourceCategory
+   */
   public IGridResource[] fetchResources( final IGridContainer parent,
                                          final IVirtualOrganization vo,
                                          final IGridResourceCategory category,
--- plugins/eu.geclipse.core/src/eu/geclipse/core/model/impl/AbstractGridInfoService.java
+++ plugins/eu.geclipse.core/src/eu/geclipse/core/model/impl/AbstractGridInfoService.java
@@ -1,9 +1,23 @@
+/*****************************************************************************
+ * Copyright (c) 2006-2008 g-Eclipse Consortium 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Initial development of the original code was made for the
+ * g-Eclipse project founded by European Union
+ * project number: FP6-IST-034327  http://www.geclipse.eu/
+ *
+ * Contributors:
+ *    Mathias Stuempert - initial API and implementation
+ *****************************************************************************/
+
 package eu.geclipse.core.model.impl;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
 
@@ -12,12 +26,28 @@ import eu.geclipse.core.model.IGridInfoService;
 import eu.geclipse.core.model.IGridJobService;
 import eu.geclipse.core.model.IGridResource;
 import eu.geclipse.core.model.IGridResourceCategory;
-import eu.geclipse.core.model.IGridService;
 import eu.geclipse.core.model.IVirtualOrganization;
 
-public abstract class AbstractGridInfoService extends AbstractGridElement implements
-    IGridInfoService {
+/**
+ * Abstract core implementation of {@link IGridInfoService}.
+ */
+public abstract class AbstractGridInfoService
+    extends AbstractGridElement
+    implements IGridInfoService {
+  
+  /* (non-Javadoc)
+   * @see eu.geclipse.core.model.IGridInfoService#fetchResources(eu.geclipse.core.model.IGridContainer, eu.geclipse.core.model.IVirtualOrganization, eu.geclipse.core.model.IGridResourceCategory, org.eclipse.core.runtime.IProgressMonitor)
+   */
+  public IGridResource[] fetchResources( final IGridContainer parent,
+                                         final IVirtualOrganization vo,
+                                         final IGridResourceCategory category,
+                                         final IProgressMonitor monitor ) {
+    return fetchResources( parent, vo, category, false, null, monitor );
+  }
 
+  /* (non-Javadoc)
+   * @see eu.geclipse.core.model.IGridInfoService#fetchResources(eu.geclipse.core.model.IGridContainer, eu.geclipse.core.model.IVirtualOrganization, eu.geclipse.core.model.IGridResourceCategory, boolean, java.lang.Class, org.eclipse.core.runtime.IProgressMonitor)
+   */
   public IGridResource[] fetchResources( final IGridContainer parent,
                                          final IVirtualOrganization vo,
                                          final IGridResourceCategory category,
