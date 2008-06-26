b443881b7ee0504d7cca6b052271495a86eb02af
Ashish Thandavan <athandava>
2008-06-26 07:49:39 +0000

    integrated David's patch that fixes the problem described in bug 229578. (David's patch was entered in bug 238494) Both have been marked as fixed and closed.

--- plugins/eu.geclipse.workflow.ui/src/eu/geclipse/workflow/ui/part/GetJobDescriptionFromFileAction.java
+++ plugins/eu.geclipse.workflow.ui/src/eu/geclipse/workflow/ui/part/GetJobDescriptionFromFileAction.java
@@ -12,6 +12,7 @@
  * Contributors:
  *     RUR (http://acet.rdg.ac.uk/)
  *     - Ashish Thandavan - initial API and implementation
+ *     - David Johnson
  ******************************************************************************/
 package eu.geclipse.workflow.ui.part;
 
@@ -19,8 +20,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import org.eclipse.core.commands.ExecutionException;
 
+import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.operations.OperationHistoryFactory;
 import org.eclipse.core.filesystem.EFS;
 import org.eclipse.core.filesystem.IFileStore;
@@ -43,152 +44,133 @@ import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
 
 import eu.geclipse.ui.dialogs.GridFileDialog;
+import eu.geclipse.workflow.IWorkflowJob;
 import eu.geclipse.workflow.IWorkflowPackage;
 import eu.geclipse.workflow.ui.edit.parts.WorkflowJobEditPart;
 import eu.geclipse.workflow.ui.internal.WorkflowDiagramEditorPlugin;
 
-
 public class GetJobDescriptionFromFileAction implements IObjectActionDelegate {
 
   /**
-   * The WorkflowJobEditPart that has been selected. 
+   * The WorkflowJobEditPart that has been selected.
    */
   private WorkflowJobEditPart mySelectedElement;
-  
   /**
    * 
    */
   private Shell myShell;
-  
   String jobDescriptionInJSDL = null;
 
-  
   public void setActivePart( IAction action, IWorkbenchPart targetPart ) {
     myShell = targetPart.getSite().getShell();
   }
 
-
   /**
-   * Fires up a GridFileDialog and fetches the contents of a user-chosen JSDL file.  
+   * Fires up a GridFileDialog and fetches the contents of a user-chosen JSDL
+   * file.
    */
   public void run( IAction action ) {
     InputStream inStream = null;
-    
     IStatus status = Status.OK_STATUS;
-    
-    GridFileDialog dialog = new GridFileDialog( myShell, GridFileDialog.STYLE_ALLOW_ONLY_LOCAL | GridFileDialog.STYLE_ALLOW_ONLY_EXISTING );
-    dialog.addFileTypeFilter( "jsdl", Messages.getString( "GetJobDescriptionFromFileAction.filterTypeDesc" ) );  //$NON-NLS-1$//$NON-NLS-2$
-
-    if ( dialog.open() == Window.OK ) {
+    GridFileDialog dialog = new GridFileDialog( myShell,
+                                                GridFileDialog.STYLE_ALLOW_ONLY_LOCAL
+                                                    | GridFileDialog.STYLE_ALLOW_ONLY_EXISTING );
+    dialog.addFileTypeFilter( "jsdl", Messages.getString( "GetJobDescriptionFromFileAction.filterTypeDesc" ) ); //$NON-NLS-1$//$NON-NLS-2$
+    if( dialog.open() == Window.OK ) {
       IFileStore[] result = dialog.getSelectedFileStores();
-
-      if ( ( result != null ) && ( result.length > 0 ) ) {
-         try {
-           inStream = result[ 0 ].openInputStream( EFS.NONE, null );
-         } catch( CoreException cE ) {
-           status = new Status( IStatus.ERROR,
-                                WorkflowDiagramEditorPlugin.ID,
-                                IStatus.OK,
-                                Messages.getString( "GetJobDescriptionFromFileAction.errorOpeningInputStream" ), //$NON-NLS-1$
-                                cE );
-         }
-         
-         if ( status.isOK() ) {
-           // Read in contents of the JSDL file
-           BufferedReader in = new BufferedReader(new InputStreamReader(inStream));
-           StringBuffer buffer = new StringBuffer();
-
-           String line = null;
-          
-           try {
-             while ((line = in.readLine()) != null) {
-               buffer.append(line);
-             }
-           } catch( IOException iOE ) {
-             status = new Status( IStatus.ERROR,
-                                  WorkflowDiagramEditorPlugin.ID,
-                                  IStatus.OK,
-                                  Messages.getString( "GetJobDescriptionFromFileAction.errorReadingFromFile" ), //$NON-NLS-1$
-                                  iOE );
-           }
-
-           jobDescriptionInJSDL = buffer.toString();
-         }
-           
-         try {
-           inStream.close();
-         } catch( IOException iOE ) {
-           status = new Status( IStatus.ERROR,
-                                WorkflowDiagramEditorPlugin.ID,
-                                IStatus.OK,
-                                Messages.getString( "GetJobDescriptionFromFileAction.errorClosingInputStream" ), //$NON-NLS-1$
-                                iOE );
+      if( ( result != null ) && ( result.length > 0 ) ) {
+        try {
+          inStream = result[ 0 ].openInputStream( EFS.NONE, null );
+        } catch( CoreException cE ) {
+          status = new Status( IStatus.ERROR,
+                               WorkflowDiagramEditorPlugin.ID,
+                               IStatus.OK,
+                               Messages.getString( "GetJobDescriptionFromFileAction.errorOpeningInputStream" ), //$NON-NLS-1$
+                               cE );
         }
-
+        if( status.isOK() ) {
+          // Read in contents of the JSDL file
+          BufferedReader in = new BufferedReader( new InputStreamReader( inStream ) );
+          StringBuffer buffer = new StringBuffer();
+          String line = null;
+          try {
+            while( ( line = in.readLine() ) != null ) {
+              buffer.append( line );
+            }
+          } catch( IOException iOE ) {
+            status = new Status( IStatus.ERROR,
+                                 WorkflowDiagramEditorPlugin.ID,
+                                 IStatus.OK,
+                                 Messages.getString( "GetJobDescriptionFromFileAction.errorReadingFromFile" ), //$NON-NLS-1$
+                                 iOE );
+          }
+          jobDescriptionInJSDL = buffer.toString();
+        }
+        try {
+          inStream.close();
+        } catch( IOException iOE ) {
+          status = new Status( IStatus.ERROR,
+                               WorkflowDiagramEditorPlugin.ID,
+                               IStatus.OK,
+                               Messages.getString( "GetJobDescriptionFromFileAction.errorClosingInputStream" ), //$NON-NLS-1$
+                               iOE );
+        }
       }
     }
-    
     updateWorkflowJobDescription();
   }
-  
+
   /**
    * This method updates the JobDescription field in the WorkflowJob.
    */
   public boolean updateWorkflowJobDescription() {
-    
     IStatus status = Status.OK_STATUS;
-
-//    System.out.println( mySelectedElement.toString() );
-//    System.out.println( mySelectedElement.getEditingDomain().toString() );
-    
     TransactionalEditingDomain domain = mySelectedElement.getEditingDomain();
     ResourceSet resourceSet = domain.getResourceSet();
-    
- 
     AbstractTransactionalCommand command = new AbstractTransactionalCommand( domain,
-                                                                             Messages.getString("GetJobDescriptionFromFileAction.updatingJobDescription"), //$NON-NLS-1$
+                                                                             Messages.getString( "GetJobDescriptionFromFileAction.updatingJobDescription" ), //$NON-NLS-1$
                                                                              null )
     {
+
       @Override
-      protected CommandResult doExecuteWithResult( IProgressMonitor monitor, IAdaptable info )
+      protected CommandResult doExecuteWithResult( IProgressMonitor monitor,
+                                                   IAdaptable info )
       {
-        mySelectedElement.getNotationView().eSet( IWorkflowPackage.eINSTANCE.getIWorkflowJob_JobDescription(), jobDescriptionInJSDL );
-        
-        
+        IWorkflowJob job = ( IWorkflowJob )mySelectedElement.resolveSemanticElement();
+        job.setJobDescription( jobDescriptionInJSDL );
         return CommandResult.newOKCommandResult();
       }
-
-
     };
-    
     try {
-      OperationHistoryFactory.getOperationHistory().execute( command, new NullProgressMonitor(), null );
+      OperationHistoryFactory.getOperationHistory()
+        .execute( command, new NullProgressMonitor(), null );
     } catch( ExecutionException eE ) {
       status = new Status( IStatus.ERROR,
                            WorkflowDiagramEditorPlugin.ID,
                            IStatus.OK,
                            Messages.getString( "GetJobDescriptionFromFileAction.errorUpdatingJobDescription" ), //$NON-NLS-1$
                            eE );
-//      WorkflowDiagramEditorPlugin.getInstance()
-//        .logError( Messages.getString("GetJobDescriptionFromFileAction.errorUpdatingJobDescription"), e ); //$NON-NLS-1$
-    } 
+      // WorkflowDiagramEditorPlugin.getInstance()
+      // .logError(
+      // Messages.getString("GetJobDescriptionFromFileAction.errorUpdatingJobDescription"),
+      // e ); //$NON-NLS-1$
+    }
     return true;
-    
-     
   }
-  
 
   public void selectionChanged( IAction action, ISelection selection ) {
     mySelectedElement = null;
     if( selection instanceof IStructuredSelection ) {
       IStructuredSelection structuredSelection = ( IStructuredSelection )selection;
-      if( structuredSelection.size() == 1 && structuredSelection.getFirstElement() instanceof WorkflowJobEditPart ) {
+      if( structuredSelection.size() == 1
+          && structuredSelection.getFirstElement() instanceof WorkflowJobEditPart )
+      {
         mySelectedElement = ( WorkflowJobEditPart )structuredSelection.getFirstElement();
       }
     }
     action.setEnabled( isEnabled() );
   }
-  
+
   private boolean isEnabled() {
     return mySelectedElement != null;
   }
