10357ce49c818681f949de3fbfa329c9c3d3504f
Katarzyna Bylec <katarzyna.bylec@gmail.com>
2008-07-16 08:24:15 +0000

    new class for IGridApplication

---
+++ plugins/eu.geclipse.info/src/eu/geclipse/info/model/GridGlueServiceApplication.java
@@ -0,0 +1,102 @@
+package eu.geclipse.info.model;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+
+import eu.geclipse.core.model.IGridApplication;
+import eu.geclipse.core.model.IGridApplicationParameters;
+import eu.geclipse.core.model.IGridComputing;
+import eu.geclipse.core.model.IGridContainer;
+import eu.geclipse.info.glue.GlueService;
+
+
+/**
+ * @author tnikos
+ *
+ */
+public class GridGlueServiceApplication extends GridGlueElement
+  implements IGridApplication
+{
+  private ArrayList<IGridComputing> computing = new ArrayList<IGridComputing>();
+  /**
+   * @param parent the parent of the element
+   * @param glueService the GlueService that describes the GRIA application
+   */
+  public GridGlueServiceApplication( final IGridContainer parent,
+                                     final GlueService glueService )
+  {
+    super( parent, glueService );
+  }
+
+  /**
+   * Add a computing resource
+   * @param myComputing an IGridComputing where this application is installed
+   */
+  public void addComputing(final IGridComputing myComputing)
+  {
+    if (myComputing != null)
+      this.computing.add( myComputing );
+  }
+  
+  public IGridComputing[] getComputing() {
+    return this.computing.toArray( new IGridComputing[this.computing.size()] );
+  }
+
+  public URI getScript() {
+    // TODO do nothing
+    return null;
+  }
+
+  public String getTag() {
+    // TODO do nothing
+    return null;
+  }
+
+  public void setScript( final URI script ) {
+    // do nothing
+  }
+
+  public String getHostName() {
+    String str = null;
+    URI myURI = null;
+    try {
+      myURI = new URI(this.getGlueApplication().endpoint);
+      str = myURI.getHost();
+    } catch( URISyntaxException e ) {
+      //do nothing
+    }
+    return str;
+  }
+
+  public URI getURI() {
+    URI uri = null;
+    try {
+      uri = new URI( getGlueApplication().uri );
+    } catch ( URISyntaxException uriExc ) {
+      // Nothing to do, just catch and return null
+    }
+    return uri;
+  }
+  
+  /**
+   * Returns the GlueService that was passed in the constructor
+   * @return a GlueService object or <code>null</code>
+   */
+  public GlueService getGlueApplication() {
+    return ( GlueService ) getGlueElement();
+  }
+  
+  /**
+   * Get the name of the computing
+   */
+  @Override
+  public String getName() {
+    return getGlueApplication().name;
+  }
+
+  public IGridApplicationParameters getApplicationParameters() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+}
