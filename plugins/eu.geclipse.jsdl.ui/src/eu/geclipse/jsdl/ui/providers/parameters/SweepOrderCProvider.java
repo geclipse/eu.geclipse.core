74db3db8b3cd57232788b5faf9817eaf41e2d5e6
Katarzyna Bylec <katarzyna.bylec@gmail.com>
2008-08-25 14:46:31 +0000

    UI support for parametric JSDL

---
+++ plugins/eu.geclipse.jsdl.ui/src/eu/geclipse/jsdl/ui/providers/parameters/SweepOrderCProvider.java
@@ -0,0 +1,84 @@
+/******************************************************************************
+ * Copyright (c) 2008 g-Eclipse consortium 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Initial development of the original code was made for
+ * project g-Eclipse founded by European Union
+ * project number: FP6-IST-034327  http://www.geclipse.eu/
+ *
+ * Contributor(s):
+ *     PSNC: 
+ *      - Katarzyna Bylec (katis@man.poznan.pl)
+ *           
+ *****************************************************************************/
+package eu.geclipse.jsdl.ui.providers.parameters;
+
+import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.jface.viewers.Viewer;
+
+import eu.geclipse.jsdl.model.sweep.SweepType;
+
+public class SweepOrderCProvider implements ITreeContentProvider {
+
+  public Object[] getChildren( final Object parentElement ) {
+    Object[] result = null;
+    if( parentElement instanceof SweepType ) {
+      SweepType sweep = ( SweepType )parentElement;
+      if( sweep.getSweep() != null && sweep.getSweep().size() != 0 ) {
+        result = new Object[ sweep.getAssignment().size() ];
+        result = sweep.getSweep().toArray( result );
+      }
+    }
+    return result;
+  }
+
+  public Object getParent( final Object element ) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public boolean hasChildren( final Object element ) {
+    boolean result = false;
+    if( element instanceof SweepType ) {
+      SweepType sweep = ( SweepType )element;
+      if( sweep.getSweep() != null && sweep.getSweep().size() != 0 ) {
+        result = true;
+      }
+    }
+    return result;
+  }
+
+  public Object[] getElements( final Object inputElement ) {
+    Object[] result = null;
+    if( inputElement instanceof SweepType[] ) {
+      
+//      SweepType sweep = (SweepType) inputElement;
+//      result = new Object[((AssignmentType[])inputElement).length];
+//      result = ( Object[] )inputElement;
+//      result = new Object[1];
+//      result[0] = sweep;
+      result = new Object[((Object[])inputElement).length];
+      result = ( Object[] )inputElement;
+    } else if (inputElement instanceof SweepType){
+      SweepType sweep = (SweepType) inputElement;
+      result = new Object[1];
+      result[0] = sweep;
+//      ??
+    }
+    return result;
+  }
+
+  public void dispose() {
+    // TODO Auto-generated method stub
+  }
+
+  public void inputChanged( final Viewer viewer,
+                            final Object oldInput,
+                            final Object newInput )
+  {
+    viewer.refresh();
+  }
+}
---
+++ plugins/eu.geclipse.jsdl.ui/src/eu/geclipse/jsdl/ui/providers/parameters/SweepOrderLProvider.java
@@ -0,0 +1,54 @@
+/******************************************************************************
+ * Copyright (c) 2008 g-Eclipse consortium 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Initial development of the original code was made for
+ * project g-Eclipse founded by European Union
+ * project number: FP6-IST-034327  http://www.geclipse.eu/
+ *
+ * Contributor(s):
+ *     PSNC: 
+ *      - Katarzyna Bylec (katis@man.poznan.pl)
+ *           
+ *****************************************************************************/
+package eu.geclipse.jsdl.ui.providers.parameters;
+
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.jface.viewers.LabelProvider;
+
+import eu.geclipse.jsdl.model.sweep.AssignmentType;
+import eu.geclipse.jsdl.model.sweep.SweepType;
+
+public class SweepOrderLProvider extends LabelProvider {
+
+  @Override
+  public String getText( final Object element ) {
+    String result = "";
+    if( element instanceof SweepType ) {
+      SweepType sweep = ( SweepType )element;
+      EList list = sweep.getAssignment();
+      for( int i = 0; i < list.size(); i++ ) {
+        Object el = list.get( i );
+        if( el instanceof AssignmentType ) {
+          AssignmentType assignment = ( AssignmentType )el;
+          EList paramList = assignment.getParameter();
+          for( int j = 0; j < paramList.size(); j++ ) {
+            String name = ( String )paramList.get( j );
+            int index = name.lastIndexOf( ":" );
+            if( index != -1 ) {
+              name = name.substring( index + 1, name.length() );
+            }
+            result = result + ", " + name; //$NON-NLS-1$
+          }
+        }
+      }
+    }
+    if( result.startsWith( "," ) ) { //$NON-NLS-1$
+      result = result.substring( 2 );
+    }
+    return result;
+  }
+}
