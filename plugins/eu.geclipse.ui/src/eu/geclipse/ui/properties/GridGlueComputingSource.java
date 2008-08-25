7592367960e913c421f63c3033bf3b33e54d775b
Ariel Garcia <agarcia>
2008-08-25 16:43:54 +0000

    Change method name to lowercase (detected by findbugs) and formatting

--- plugins/eu.geclipse.ui/src/eu/geclipse/ui/properties/GridGlueComputingSource.java
+++ plugins/eu.geclipse.ui/src/eu/geclipse/ui/properties/GridGlueComputingSource.java
@@ -1,5 +1,5 @@
 /*****************************************************************************
- * Copyright (c) 2006, 2007 g-Eclipse Consortium 
+ * Copyright (c) 2006-2008 g-Eclipse Consortium 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -26,16 +26,17 @@ import eu.geclipse.info.model.GridGlueComputing;
  * @author tnikos
  *
  */
-public class GridGlueComputingSource extends AbstractPropertySource<GridGlueComputing>{
+public class GridGlueComputingSource
+    extends AbstractPropertySource< GridGlueComputing >
+{
 
-  static private List<IProperty<GridGlueComputing>> staticDescriptors;
+  static private List< IProperty< GridGlueComputing > > staticDescriptors;
   
   /**
    * @param gridGlueComputing
    */
-  public GridGlueComputingSource(final GridGlueComputing gridGlueComputing)
-  {
-    super(gridGlueComputing);
+  public GridGlueComputingSource( final GridGlueComputing gridGlueComputing ) {
+    super( gridGlueComputing );
   }
   
   public Object getEditableValue() {
@@ -43,77 +44,72 @@ public class GridGlueComputingSource extends AbstractPropertySource<GridGlueComp
   }
   
   @Override
-  protected Class<? extends AbstractPropertySource<?>> getPropertySourceClass()
-  {
+  protected Class< ? extends AbstractPropertySource < ? > > getPropertySourceClass() {
     return GridGlueComputingSource.class;
   }
 
   @Override
-  protected List<IProperty<GridGlueComputing>> getStaticProperties() {
+  protected List< IProperty< GridGlueComputing > > getStaticProperties() {
     
-    if( staticDescriptors == null ) {
+    if ( staticDescriptors == null ) {
       staticDescriptors = createProperties();
     }
     
     return staticDescriptors;
   }
   
-  static private List<IProperty<GridGlueComputing>> createProperties() {
-    List<IProperty<GridGlueComputing>> propertiesList = new ArrayList<IProperty<GridGlueComputing>>( 1 );
+  static private List< IProperty< GridGlueComputing > > createProperties() {
+    List< IProperty< GridGlueComputing > > propertiesList
+      = new ArrayList< IProperty< GridGlueComputing > >( 1 );
     propertiesList.add( createHostName() );
     propertiesList.add( createInformationServiceURL() );
-    propertiesList.add( Status() );
+    propertiesList.add( status() );
     propertiesList.add( LRMSType() );
     propertiesList.add( LRMSVersion() );
     
     return propertiesList;
   }
   
-  static private IProperty<GridGlueComputing> createHostName() {
-    return new AbstractProperty<GridGlueComputing>( Messages.getString("InfoComputingElement.hostname"), null) { //$NON-NLS-1$
+  static private IProperty< GridGlueComputing > createHostName() {
+    return new AbstractProperty< GridGlueComputing >( Messages.getString("InfoComputingElement.hostname"), null ) { //$NON-NLS-1$
       @Override
-      public Object getValue( final GridGlueComputing gridGlueComputing )
-      {
+      public Object getValue( final GridGlueComputing gridGlueComputing ) {
         return gridGlueComputing.getGlueCe().HostName;
       } 
     };
   }
   
-  static private IProperty<GridGlueComputing> createInformationServiceURL() {
-    return new AbstractProperty<GridGlueComputing>( Messages.getString("InfoComputingElement.informationServiceURL"), null) { //$NON-NLS-1$
+  static private IProperty< GridGlueComputing > createInformationServiceURL() {
+    return new AbstractProperty< GridGlueComputing >( Messages.getString("InfoComputingElement.informationServiceURL"), null ) { //$NON-NLS-1$
       @Override
-      public Object getValue( final GridGlueComputing gridGlueComputing )
-      {
+      public Object getValue( final GridGlueComputing gridGlueComputing ) {
         return gridGlueComputing.getGlueCe().InformationServiceURL;
       } 
     };
   }
   
-  static private IProperty<GridGlueComputing> Status() {
-    return new AbstractProperty<GridGlueComputing>( Messages.getString("InfoComputingElement.status"), null) { //$NON-NLS-1$
+  static private IProperty< GridGlueComputing > status() {
+    return new AbstractProperty< GridGlueComputing >( Messages.getString("InfoComputingElement.status"), null ) { //$NON-NLS-1$
       @Override
-      public Object getValue( final GridGlueComputing gridGlueComputing )
-      {
+      public Object getValue( final GridGlueComputing gridGlueComputing ) {
         return gridGlueComputing.getGlueCe().Status;
       } 
     };
   }
   
-  static private IProperty<GridGlueComputing> LRMSType() {
-    return new AbstractProperty<GridGlueComputing>( Messages.getString("InfoComputingElement.LRMSType"), null) { //$NON-NLS-1$
+  static private IProperty< GridGlueComputing > LRMSType() {
+    return new AbstractProperty< GridGlueComputing >( Messages.getString("InfoComputingElement.LRMSType"), null ) { //$NON-NLS-1$
       @Override
-      public Object getValue( final GridGlueComputing gridGlueComputing )
-      {
+      public Object getValue( final GridGlueComputing gridGlueComputing ) {
         return gridGlueComputing.getGlueCe().LRMSType;
       } 
     };
   }
     
-    static private IProperty<GridGlueComputing> LRMSVersion() {
-      return new AbstractProperty<GridGlueComputing>( Messages.getString("InfoComputingElement.LRMSVersion"), null) { //$NON-NLS-1$
+    static private IProperty< GridGlueComputing > LRMSVersion() {
+      return new AbstractProperty< GridGlueComputing >( Messages.getString("InfoComputingElement.LRMSVersion"), null ) { //$NON-NLS-1$
         @Override
-        public Object getValue( final GridGlueComputing gridGlueComputing )
-        {
+        public Object getValue( final GridGlueComputing gridGlueComputing ) {
           return gridGlueComputing.getGlueCe().LRMSVersion;
         } 
       };
