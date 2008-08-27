7abff6cc3416a7265834cf15523015fdf8e9fc95
Ariel Garcia <agarcia>
2008-08-27 12:58:22 +0000

    Remove empty comment

--- plugins/eu.geclipse.ui/src/eu/geclipse/ui/properties/PropertySourceProxy.java
+++ plugins/eu.geclipse.ui/src/eu/geclipse/ui/properties/PropertySourceProxy.java
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright (c) 2007 g-Eclipse consortium 
+ * Copyright (c) 2007-2008 g-Eclipse consortium 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -23,9 +23,6 @@ import org.eclipse.ui.views.properties.IPropertyDescriptor;
 import org.eclipse.ui.views.properties.IPropertySource;
 
 
-/**
- *
- */
 class PropertySourceProxy implements IPropertySource {
   private final List<AbstractPropertySource<?>> propertySourcesList = new ArrayList<AbstractPropertySource<?>>();
   private final Object sourceObject;
