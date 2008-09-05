7ce5968b4f52ecafe8733f1a650b3861f3f6b2f7
Ariel Garcia <agarcia>
2008-09-05 19:19:03 +0000

    Restore copyright block

--- plugins/eu.geclipse.core/src/eu/geclipse/core/model/IServiceJob.java
+++ plugins/eu.geclipse.core/src/eu/geclipse/core/model/IServiceJob.java
@@ -1,11 +1,18 @@
 /******************************************************************************
- * Copyright (c) 2007 - 2008 g-Eclipse consortium All rights reserved. This
- * program and the accompanying materials are made available under the terms of
- * the Eclipse Public License v1.0 which accompanies this distribution, and is
- * available at http://www.eclipse.org/legal/epl-v10.html Initial development of
- * the original code was made for project g-Eclipse founded by European Union
- * project number: FP6-IST-034327 http://www.geclipse.eu/ Contributor(s): PSNC:
- * - Katarzyna Bylec (katis@man.poznan.pl) - Szymon Mueller
+ * Copyright (c) 2007 - 2008 g-Eclipse consortium 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Initial development of the original code was made for
+ * project g-Eclipse founded by European Union
+ * project number: FP6-IST-034327  http://www.geclipse.eu/
+ *
+ * Contributor(s):
+ *     PSNC:
+ *      - Katarzyna Bylec (katis@man.poznan.pl)
+ *      - Szymon Mueller
  *****************************************************************************/
 package eu.geclipse.core.model;
 
