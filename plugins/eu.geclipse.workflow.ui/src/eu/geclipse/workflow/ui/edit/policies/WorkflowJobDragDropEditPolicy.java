e932a17bb75b06c9c7fcdd1017983af11826c3a5
Ashish Thandavan <athandava>
2008-09-20 22:14:39 +0000

    committing patch contributed by David against bug 247270

--- plugins/eu.geclipse.workflow.ui/src/eu/geclipse/workflow/ui/edit/policies/WorkflowJobDragDropEditPolicy.java
+++ plugins/eu.geclipse.workflow.ui/src/eu/geclipse/workflow/ui/edit/policies/WorkflowJobDragDropEditPolicy.java
@@ -15,37 +15,22 @@
  ******************************************************************************/
 package eu.geclipse.workflow.ui.edit.policies;
 
-import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
-import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.gef.commands.Command;
 import org.eclipse.gef.commands.CompoundCommand;
-import org.eclipse.gmf.runtime.diagram.core.edithelpers.CreateElementRequestAdapter;
 import org.eclipse.gmf.runtime.diagram.ui.commands.ICommandProxy;
 import org.eclipse.gmf.runtime.diagram.ui.editpolicies.DragDropEditPolicy;
-import org.eclipse.gmf.runtime.diagram.ui.editpolicies.EditPolicyRoles;
-import org.eclipse.gmf.runtime.diagram.ui.requests.CreateViewAndElementRequest;
 import org.eclipse.gmf.runtime.diagram.ui.requests.DropObjectsRequest;
-import org.eclipse.gmf.runtime.diagram.ui.requests.CreateViewAndElementRequest.ViewAndElementDescriptor;
-import org.eclipse.gmf.runtime.emf.type.core.IElementType;
-import org.eclipse.gmf.runtime.emf.type.core.IHintedType;
-import org.eclipse.gmf.runtime.emf.type.core.requests.CreateElementRequest;
-import org.eclipse.gmf.runtime.emf.type.core.requests.DestroyElementRequest;
-import org.eclipse.gmf.runtime.notation.Node;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.dialogs.MessageDialog;
 
 import eu.geclipse.jsdl.JSDLJobDescription;
+import eu.geclipse.workflow.model.IWorkflowJob;
 import eu.geclipse.workflow.ui.edit.commands.CopyJobDescToWorkflowCommand;
-import eu.geclipse.workflow.ui.edit.commands.InputPortAfterCreateCommand;
-import eu.geclipse.workflow.ui.edit.commands.OutputPortAfterCreateCommand;
 import eu.geclipse.workflow.ui.edit.commands.UpdateJobPortsCommand;
-import eu.geclipse.workflow.ui.edit.parts.InputPortEditPart;
-import eu.geclipse.workflow.ui.edit.parts.OutputPortEditPart;
 import eu.geclipse.workflow.ui.edit.parts.WorkflowJobEditPart;
-import eu.geclipse.workflow.ui.providers.WorkflowElementTypes;
+import eu.geclipse.workflow.ui.part.Messages;
 
 /**
  * @author david
@@ -73,15 +58,32 @@ public class WorkflowJobDragDropEditPolicy extends DragDropEditPolicy {
       if( o instanceof JSDLJobDescription ) {
         jsdl = ( JSDLJobDescription )o;
         this.selectedElement = ( WorkflowJobEditPart )getHost();
+        IWorkflowJob selectedJob = (IWorkflowJob)this.selectedElement.resolveSemanticElement();
+        
         CopyJobDescToWorkflowCommand copyCmd = new CopyJobDescToWorkflowCommand( this.selectedElement.resolveSemanticElement(), jsdl);
-        cmd.add( new ICommandProxy(copyCmd) );        
         UpdateJobPortsCommand updatePortsCmd = new UpdateJobPortsCommand(this.selectedElement, jsdl);
-        cmd.add (new ICommandProxy(updatePortsCmd) );        
+        
+        if (!(selectedJob.getName()==null && selectedJob.getJobDescription()==null)) {
+          MessageDialog confirmDialog = new MessageDialog( null,
+               Messages.getString("WorkflowJobDragDropEditPolicy_confirmationTitle"), //$NON-NLS-1$
+               null,
+               Messages.getString( "WorkflowJobDragDropEditPolicy_userPrompt" ), //$NON-NLS-1$
+               true ? MessageDialog.QUESTION : MessageDialog.WARNING ,
+               new String[] { IDialogConstants.YES_LABEL, IDialogConstants.NO_LABEL },
+               0 );
+          int result = confirmDialog.open();
+          if (result==0) {
+            cmd.add( new ICommandProxy(copyCmd) );        
+            cmd.add (new ICommandProxy(updatePortsCmd) ); 
+          }
+        } else {
+          cmd.add( new ICommandProxy(copyCmd) );        
+          cmd.add (new ICommandProxy(updatePortsCmd) ); 
+        }      
       }
 
       return cmd;
     }
     return super.getDropObjectsCommand( dropRequest );
-  }
-  
+  }   
 }
