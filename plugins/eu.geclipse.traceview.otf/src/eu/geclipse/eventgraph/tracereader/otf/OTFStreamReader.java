81ac770a01aeeb24682246e502e0709cfcaabd12
Thomas Köckerbauer <tkoeck@gup.jku.at>
2008-12-09 14:49:22 +0000

    added support for compressed otf files (might throw an exception because of java bug # 4040920, the jcraft inflater would solve the problem)

--- plugins/eu.geclipse.traceview.otf/src/eu/geclipse/eventgraph/tracereader/otf/OTFStreamReader.java
+++ plugins/eu.geclipse.traceview.otf/src/eu/geclipse/eventgraph/tracereader/otf/OTFStreamReader.java
@@ -2,8 +2,13 @@ package eu.geclipse.eventgraph.tracereader.otf;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.zip.InflaterInputStream;
+
+//import com.jcraft.jzlib.ZInputStream;
 
 import eu.geclipse.traceview.EventType;
 
@@ -16,7 +21,15 @@ class OTFStreamReader extends OTFUtils {
   OTFStreamReader( final File file, final OTFReader trace ) throws IOException {
 //System.out.println( "Opening " + file );
     this.trace = trace;
-    this.input = new BufferedReader( new FileReader( file ) );
+    if (file.exists()) {
+      this.input = new BufferedReader( new FileReader( file ) );      
+    } else {
+      File zFile = new File(file.getParentFile(), file.getName()+".z"); //$NON-NLS-1$
+      // jcraft inflater
+//      this.input = new BufferedReader( new InputStreamReader (new ZInputStream( new FileInputStream( zFile ) ) ) );
+      // SUN inflater, suffers from java bug # 4040920
+      this.input = new BufferedReader( new InputStreamReader (new InflaterInputStream( new FileInputStream( zFile ) ) ) );
+    }
     while( ( this.line = this.input.readLine() ) != null ) {
       this.lineIdx = 0;
       int ch = this.line.charAt( 0 );
