7bf1cbb9bc5b388d31b7ea5ab32e1d49511489aa
Nicholas Loulloudes <loulloudes.n@cs.ucy.ac.cy>
2008-12-10 18:04:32 +0000

    Commiting Patch for bug#: 254882

--- plugins/eu.geclipse.ui/src/eu/geclipse/ui/internal/actions/TransformAction.java
+++ plugins/eu.geclipse.ui/src/eu/geclipse/ui/internal/actions/TransformAction.java
@@ -30,6 +30,7 @@ import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.action.Action;
+import org.eclipse.swt.SWT;
 import org.eclipse.ui.IWorkbenchSite;
 
 import eu.geclipse.core.model.IGridContainer;
@@ -61,19 +62,82 @@ public class TransformAction extends Action {
   
   @Override
   public void run() {
-    WorkspaceJob job = new WorkspaceJob( "transformer" ) { //$NON-NLS-1$
-      @Override
-      public IStatus runInWorkspace( final IProgressMonitor monitor )
-          throws CoreException {
-        return transform( TransformAction.this.creator, TransformAction.this.input, monitor );
+    
+ for (IGridJobDescription description : this.input ){
+      
+      IFile file = getFile( description );
+      
+      if ( file.exists() ){
+        FileOverwriteDialog dialog = new FileOverwriteDialog( this.site.getShell(), file.getName() );
+        if (dialog.open() == SWT.YES ){        
+          try {
+            file.delete( true, null );
+            
+            WorkspaceJob job = new WorkspaceJob( "transformer" ) { //$NON-NLS-1$
+              @Override
+              public IStatus runInWorkspace( final IProgressMonitor monitor )
+                  throws CoreException {
+                return transform( TransformAction.this.creator, TransformAction.this.input, monitor );
+              }
+            };
+            job.setUser( true );
+            job.schedule();
+          } catch( CoreException e ) {
+            Activator.logException( e );
+        }
       }
-    };
-    job.setUser( true );
-    job.schedule();
+    }
+ }
+    
+
      
   }
   
   protected IStatus transform( final IGridElementCreator creator,
+                               final IGridJobDescription[] input,
+                               final IProgressMonitor monitor ) {
+
+    IStatus result = Status.OK_STATUS;
+    List< IStatus > errors = new ArrayList< IStatus >();
+    
+    IProgressMonitor lMonitor
+      = monitor == null
+      ? new NullProgressMonitor()
+      : monitor;
+      
+    lMonitor.beginTask( "Transforming job descriptions", input.length );
+    
+    try {
+      for ( IGridJobDescription description : input ) {
+        lMonitor.subTask( description.getName() );
+        IStatus status = transform( creator, description );
+        if ( ! status.isOK() ) {
+          errors.add( status );
+        }
+        lMonitor.worked( 1 );
+      }
+    } finally {
+      lMonitor.done();
+    }
+    
+    if ( ( errors != null ) && ( errors.size() > 0 ) ) {
+      if ( errors.size() == 1 ) {
+        result = errors.get( 0 );
+      } else {
+        result = new MultiStatus(
+            Activator.PLUGIN_ID,
+            0,
+            errors.toArray( new IStatus[ errors.size() ] ),
+            "Transformation problems",
+            null
+        );
+      }
+    }
+    
+    return result;
+  }
+  
+  protected IStatus transform2( final IGridElementCreator creator,
                                final IGridJobDescription[] input,
                                final IProgressMonitor monitor ) {
 
@@ -86,8 +150,7 @@ public class TransformAction extends Action {
       
       if ( file.exists() ){
         FileOverwriteDialog dialog = new FileOverwriteDialog( this.site.getShell(), file.getName() );
-        boolean temp = dialog.open();
-        if (temp == true ){        
+        if (dialog.open() == SWT.YES){        
           try {
             file.delete( true, null );
             
