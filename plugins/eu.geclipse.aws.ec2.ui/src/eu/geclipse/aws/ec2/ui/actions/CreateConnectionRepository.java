7d5b965f3c3613121bf87c73c20f19f75d256e9b
Ariel Garcia <agarcia>
2009-03-24 04:19:20 +0000

    Tabs not allowed by our coding guidelines, remove them!

--- plugins/eu.geclipse.aws.ec2.ui/src/eu/geclipse/aws/ec2/ui/actions/CreateConnectionRepository.java
+++ plugins/eu.geclipse.aws.ec2.ui/src/eu/geclipse/aws/ec2/ui/actions/CreateConnectionRepository.java
@@ -18,97 +18,96 @@ import eu.geclipse.aws.ec2.EC2Instance;
 import eu.geclipse.core.model.IGridProject;
 import eu.geclipse.ui.actions.MountAction;
 
+
 public class CreateConnectionRepository extends AbstractAWSProjectAction {
 
-	/** The list of AMIs which are selected. */
-	private ArrayList<EC2Instance> instanceList;
+  /** The list of AMIs which are selected. */
+  private ArrayList< EC2Instance > instanceList;
 
-	/**
-	 * Instantiate and initialize the reboot object.
-	 */
-	public CreateConnectionRepository() {
-		this.instanceList = new ArrayList<EC2Instance>();
-	}
+  /**
+   * Instantiate and initialize the reboot object.
+   */
+  public CreateConnectionRepository() {
+    this.instanceList = new ArrayList< EC2Instance >();
+  }
 
-	@Override
-	public void run(final IAction action) {
-		if (action.isEnabled()) {
+  @Override
+  public void run( final IAction action ) {
+    if ( action.isEnabled() ) {
 
-			Job job = new Job("Mount a Grid connection to the instance") { //$NON-NLS-1$
+      Job job = new Job("Mount a Grid connection to the instance") { //$NON-NLS-1$
 
-				@Override
-				protected IStatus run(final IProgressMonitor monitor) {
-					try {
-						if (CreateConnectionRepository.this.instanceList.size() == 0) {
-							return Status.CANCEL_STATUS;
-						}
+        @Override
+        protected IStatus run( final IProgressMonitor monitor ) {
+          try {
+            if ( CreateConnectionRepository.this.instanceList.size() == 0 ) {
+              return Status.CANCEL_STATUS;
+            }
 
-						for (EC2Instance instance : CreateConnectionRepository.this.instanceList) {
+            for ( EC2Instance instance : CreateConnectionRepository.this.instanceList ) {
 
-							// extract the hostname from the instance
+              // extract the hostname from the instance
 
-							String hostname = instance.getHostName();
+              String hostname = instance.getHostName();
 
-							String connectionname = "repos-" + hostname ; 
-							
-							// for the Connection in the Workspace a IPath is
-							// needed
-							// 
-							IGridProject project = instance.getProject();
+              String connectionname = "repos-" + hostname ; 
 
-							IPath path = instance.getProject().getPath()
-									.append("Connections").append(connectionname);
+              // For the Connection in the Workspace an IPath is needed
+              IGridProject project = instance.getProject();
 
-							try {
-								URI uri = new URI("sftp", "root", hostname, 22,
-										"/opt/tomcat/apache-tomcat-6.0.18/webapps/ROOT/repos", null, null);
+              IPath path = instance.getProject().getPath()
+                             .append( "Connections" ).append( connectionname );
 
-								MountAction
-										.createLocalMount(uri, path, monitor);
-								System.out.println("URI: " + uri);
+              try {
+                // TODO remove the hardcoded Tomcat path!!
+                URI uri = new URI( "sftp", "root", hostname, 22,
+                                   "/opt/tomcat/apache-tomcat-6.0.18/webapps/ROOT/repos", null, null );
 
-							} catch (URISyntaxException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							} catch (CoreException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
+                MountAction.createLocalMount( uri, path, monitor );
+                System.out.println("URI: " + uri);
 
-						}
-						return Status.OK_STATUS;
+              } catch ( URISyntaxException e ) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+              } catch ( CoreException e ) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+              }
 
-					} finally {
-						monitor.done();
-					}
-				}
-			};
-			job.setPriority(Job.SHORT);
-			job.setUser(true);
-			job.schedule();
-		}
+            }
+            return Status.OK_STATUS;
 
-	}
+          } finally {
+            monitor.done();
+          }
+        }
+      };
+      job.setPriority(Job.SHORT);
+      job.setUser(true);
+      job.schedule();
+    }
 
-	public void selectionChanged(final IAction action,
-			final ISelection selection) {
-		boolean enable = false;
-		this.instanceList.clear();
+  }
 
-		if (selection instanceof IStructuredSelection) {
-			IStructuredSelection structuredSelection = (IStructuredSelection) selection;
-			for (Object element : structuredSelection.toList()) {
-				if (element instanceof EC2Instance) {
-					EC2Instance ec2Instance = (EC2Instance) element;
-					this.instanceList.add(ec2Instance);
-				}
-			}
-		}
-		if (this.instanceList.size() > 0) {
-			enable = true;
-		}
-		action.setEnabled(enable);
+  public void selectionChanged( final IAction action,
+                                final ISelection selection ) {
+    boolean enable = false;
+    this.instanceList.clear();
 
-	}
+    if ( selection instanceof IStructuredSelection ) {
+      IStructuredSelection structuredSelection = ( IStructuredSelection ) selection;
+      for ( Object element : structuredSelection.toList() ) {
+        if ( element instanceof EC2Instance ) {
+          EC2Instance ec2Instance = ( EC2Instance ) element;
+          this.instanceList.add( ec2Instance );
+        }
+      }
+    }
+    if ( this.instanceList.size() > 0 ) {
+      enable = true;
+    }
+    action.setEnabled( enable );
+
+  }
 
 }
