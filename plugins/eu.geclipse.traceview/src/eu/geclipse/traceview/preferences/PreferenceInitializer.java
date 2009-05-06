a69fbd45955b854042f0ac2af57261864de0369f
Thomas Köckerbauer <tkoeck@gup.jku.at>
2009-05-06 11:21:13 +0000

    changed preference initializer to work outside of the ui thread

--- plugins/eu.geclipse.traceview/src/eu/geclipse/traceview/preferences/PreferenceInitializer.java
+++ plugins/eu.geclipse.traceview/src/eu/geclipse/traceview/preferences/PreferenceInitializer.java
@@ -18,8 +18,7 @@ package eu.geclipse.traceview.preferences;
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.preference.PreferenceConverter;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.graphics.RGB;
 
 import eu.geclipse.traceview.internal.Activator;
 
@@ -27,6 +26,12 @@ import eu.geclipse.traceview.internal.Activator;
  * Class used to initialize default preference values.
  */
 public class PreferenceInitializer extends AbstractPreferenceInitializer {
+  private final static RGB RGB_BLACK = new RGB(0, 0, 0);
+  private final static RGB RGB_GREEN = new RGB(0, 255, 0);
+  private final static RGB RGB_BLUE = new RGB(0, 0, 255);
+  private final static RGB RGB_DARK_MAGENTA = new RGB(128, 0, 128);
+  private final static RGB RGB_YELLOW = new RGB(255, 255, 0);
+  private final static RGB RGB_GRAY = new RGB(192, 192, 192);
 
   /*
    * (non-Javadoc)
@@ -60,65 +65,45 @@ public class PreferenceInitializer extends AbstractPreferenceInitializer {
     PreferenceConverter.setDefault( store,
                                     PreferenceConstants.P_MESSAGE
                                         + PreferenceConstants.P_COLOR,
-                                    Display.getCurrent()
-                                      .getSystemColor( SWT.COLOR_BLACK )
-                                      .getRGB() );
+                                    RGB_BLACK );
     // event color
     PreferenceConverter.setDefault( store,
                                     PreferenceConstants.P_SEND_EVENT
                                         + PreferenceConstants.P_COLOR,
-                                    Display.getCurrent()
-                                      .getSystemColor( SWT.COLOR_BLACK )
-                                      .getRGB() );
+                                    RGB_BLACK );
     PreferenceConverter.setDefault( store,
                                     PreferenceConstants.P_RECV_EVENT
                                         + PreferenceConstants.P_COLOR,
-                                    Display.getCurrent()
-                                      .getSystemColor( SWT.COLOR_BLACK )
-                                      .getRGB() );
+                                    RGB_BLACK );
     PreferenceConverter.setDefault( store,
                                     PreferenceConstants.P_TEST_EVENT
                                         + PreferenceConstants.P_COLOR,
-                                    Display.getCurrent()
-                                      .getSystemColor( SWT.COLOR_GREEN )
-                                      .getRGB() );
+                                    RGB_GREEN );
     PreferenceConverter.setDefault( store,
                                     PreferenceConstants.P_OTHER_EVENT
                                         + PreferenceConstants.P_COLOR,
-                                    Display.getCurrent()
-                                      .getSystemColor( SWT.COLOR_BLUE )
-                                      .getRGB() );
+                                    RGB_BLUE );
     // event fill color
     PreferenceConverter.setDefault( store,
                                     PreferenceConstants.P_SEND_EVENT
                                         + PreferenceConstants.P_FILL_COLOR,
-                                    Display.getCurrent()
-                                      .getSystemColor( SWT.COLOR_DARK_MAGENTA )
-                                      .getRGB() );
+                                    RGB_DARK_MAGENTA );
     PreferenceConverter.setDefault( store,
                                     PreferenceConstants.P_RECV_EVENT
                                         + PreferenceConstants.P_FILL_COLOR,
-                                    Display.getCurrent()
-                                      .getSystemColor( SWT.COLOR_YELLOW )
-                                      .getRGB() );
+                                    RGB_YELLOW );
     PreferenceConverter.setDefault( store,
                                     PreferenceConstants.P_TEST_EVENT
                                         + PreferenceConstants.P_FILL_COLOR,
-                                    Display.getCurrent()
-                                      .getSystemColor( SWT.COLOR_GREEN )
-                                      .getRGB() );
+                                    RGB_GREEN);
     PreferenceConverter.setDefault( store,
                                     PreferenceConstants.P_OTHER_EVENT
                                         + PreferenceConstants.P_FILL_COLOR,
-                                    Display.getCurrent()
-                                      .getSystemColor( SWT.COLOR_BLUE )
-                                      .getRGB() );
+                                    RGB_BLUE );
     // selection Color
     PreferenceConverter.setDefault( store,
                                     PreferenceConstants.P_SELECTION_COLOR,
-                                    Display.getCurrent()
-                                      .getSystemColor( SWT.COLOR_GRAY )
-                                      .getRGB() );
+                                    RGB_GRAY );
     
     // cache dir
     store.setDefault( PreferenceConstants.P_CACHE_DIR, System.getProperty( "java.io.tmpdir" ) ); //$NON-NLS-1$
