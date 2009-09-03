9e277dc026ee88f12025c60a1d47da32d4f2674c
Thomas Köckerbauer <tkoeck@gup.jku.at>
2009-09-03 09:40:35 +0000

    applied patch in bug #288434

--- plugins/eu.geclipse.traceview/src/eu/geclipse/traceview/internal/EventMarkers.java
+++ plugins/eu.geclipse.traceview/src/eu/geclipse/traceview/internal/EventMarkers.java
@@ -11,33 +11,50 @@ import eu.geclipse.traceview.IEventMarker;
 import eu.geclipse.traceview.ITrace;
 
 class EventMarkerEntry {
+
   String id;
   boolean enabled;
   String label;
   IEventMarker marker;
+  int priority = Integer.MAX_VALUE;
 }
 
 public class EventMarkers {
+
   protected List<EventMarkerEntry> eventMarkerEntries;
   protected List<IEventMarker> eventMarkers;
 
-  public EventMarkers( ITrace trace ) {
-    if (this.eventMarkerEntries == null) {
+  public EventMarkers( final ITrace trace ) {
+    if( this.eventMarkerEntries == null ) {
       this.eventMarkerEntries = new ArrayList<EventMarkerEntry>();
       for( IConfigurationElement configurationElement : Platform.getExtensionRegistry()
-          .getConfigurationElementsFor( "eu.geclipse.traceview.EventMarker" ) ) { //$NON-NLS-1$
+        .getConfigurationElementsFor( "eu.geclipse.traceview.EventMarker" ) ) { //$NON-NLS-1$
         try {
           EventMarkerEntry entry = new EventMarkerEntry();
           entry.marker = ( IEventMarker )configurationElement.createExecutableExtension( "class" ); //$NON-NLS-1$
           entry.id = configurationElement.getAttribute( "id" ); //$NON-NLS-1$
           entry.label = configurationElement.getAttribute( "label" ); //$NON-NLS-1$
           entry.enabled = true;
+          String priority = configurationElement.getAttribute( "priority" ); //$NON-NLS-1$
+          if( priority != null ) {
+            entry.priority = Integer.parseInt( priority );
+          }
           String defaultEnabled = configurationElement.getAttribute( "defaultEnabled" ); //$NON-NLS-1$
-          if (defaultEnabled != null) {
+          if( defaultEnabled != null ) {
             entry.enabled = Boolean.parseBoolean( defaultEnabled );
           }
           entry.marker.setTrace( trace );
-          this.eventMarkerEntries.add( entry );
+          boolean added = false;
+          for( int i = 0; i < this.eventMarkerEntries.size(); i++ ) {
+            if( entry.priority < this.eventMarkerEntries.get( i ).priority ) {
+              this.eventMarkerEntries.add( i, entry );
+              added = true;
+              break;
+            }
+          }
+          if( !added ) {
+            this.eventMarkerEntries.add( entry );
+          }
         } catch( CoreException coreException ) {
           Activator.logException( coreException );
         }
@@ -48,8 +65,10 @@ public class EventMarkers {
 
   public void buildEventMarkersList() {
     this.eventMarkers = new ArrayList<IEventMarker>();
-    for (EventMarkerEntry entry : eventMarkerEntries) {
-      if (entry.enabled) this.eventMarkers.add( entry.marker );
+    for( EventMarkerEntry entry : this.eventMarkerEntries ) {
+      if( entry.enabled ) {
+        this.eventMarkers.add( entry.marker );
+      }
     }
   }
 
@@ -58,6 +77,6 @@ public class EventMarkers {
   }
 
   public List<EventMarkerEntry> getEventMarkerEntries() {
-    return eventMarkerEntries;
+    return this.eventMarkerEntries;
   }
 }
