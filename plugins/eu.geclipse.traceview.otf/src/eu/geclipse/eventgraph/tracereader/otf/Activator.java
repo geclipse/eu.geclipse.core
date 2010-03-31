3f3795d6e266b1b9c7445521b3c01a732d92ebd3
Christof Klausecker <cklausecker>
2010-03-31 13:54:43 +0000

    added logger

--- plugins/eu.geclipse.traceview.otf/src/eu/geclipse/eventgraph/tracereader/otf/Activator.java
+++ plugins/eu.geclipse.traceview.otf/src/eu/geclipse/eventgraph/tracereader/otf/Activator.java
@@ -1,5 +1,7 @@
 package eu.geclipse.eventgraph.tracereader.otf;
 
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
@@ -7,44 +9,58 @@ import org.osgi.framework.BundleContext;
  * The activator class controls the plug-in life cycle
  */
 public class Activator extends AbstractUIPlugin {
+
   /** The plug-in ID */
   public static final String PLUGIN_ID = "eu.geclipse.eventgraph.tracereader.otf"; //$NON-NLS-1$
-  
   // The shared instance
   private static Activator plugin;
-  
+
   /**
    * The constructor
    */
   public Activator() {
     plugin = this;
   }
-  
+
   /*
    * (non-Javadoc)
    * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
    */
   @Override
   public void start( final BundleContext context ) throws Exception {
-    super.start(context);
+    super.start( context );
   }
-  
+
   /*
    * (non-Javadoc)
-   * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+   * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext )
    */
   @Override
   public void stop( final BundleContext context ) throws Exception {
     plugin = null;
-    super.stop(context);
+    super.stop( context );
   }
-  
+
   /**
    * Returns the shared instance
-   *
+   * 
    * @return the shared instance
    */
   public static Activator getDefault() {
     return plugin;
+  }
+
+  /**
+   * Logs an exception.
+   * 
+   * @param exception the exception.
+   */
+  public static void logException( final Exception exception ) {
+    String message = exception.getLocalizedMessage();
+    if( message == null ) {
+      message = exception.getClass().getName();
+    }
+    IStatus status = new Status( IStatus.ERROR, PLUGIN_ID, IStatus.OK, message, exception );
+    getDefault().getLog().log( status );
   }
 }
