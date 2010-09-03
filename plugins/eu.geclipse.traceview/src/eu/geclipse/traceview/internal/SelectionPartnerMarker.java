81b3cf42a196f98a5d598f3d705b481948bd1282
Thomas Köckerbauer <tkoeck@gup.jku.at>
2010-09-03 20:58:01 +0000

    bugfix

--- plugins/eu.geclipse.traceview/src/eu/geclipse/traceview/internal/SelectionPartnerMarker.java
+++ plugins/eu.geclipse.traceview/src/eu/geclipse/traceview/internal/SelectionPartnerMarker.java
@@ -33,37 +33,44 @@ import eu.geclipse.traceview.IEventMarker;
 import eu.geclipse.traceview.utils.AbstractEventMarker;
 
 public class SelectionPartnerMarker extends AbstractEventMarker {
-  Set<IEvent> selectedEvents = new HashSet<IEvent>();
-  Set<IEvent> partnerEvents = new HashSet<IEvent>();
+  private Set<IEvent> selectedEvents = new HashSet<IEvent>();
+  private Set<IEvent> partnerEvents = new HashSet<IEvent>();
+  private final Color fgColor = Display.getDefault().getSystemColor( SWT.COLOR_BLACK );
+  private final Color bgColor = Display.getDefault().getSystemColor( SWT.COLOR_GRAY );
   
   public SelectionPartnerMarker() {
     ISelectionService selectionService = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getSelectionService();
     selectionService.addSelectionListener( "eu.geclipse.traceview.views.TraceView", new ISelectionListener() {
       public void selectionChanged( IWorkbenchPart part, ISelection selection ) {
         StructuredSelection sSel = ( StructuredSelection )selection;
-        SelectionPartnerMarker.this.selectedEvents = new HashSet<IEvent>();
-        SelectionPartnerMarker.this.partnerEvents = new HashSet<IEvent>();
+        Set<IEvent> newSelectedEvents = new HashSet<IEvent>();
+        Set<IEvent> newPartnerEvents = new HashSet<IEvent>();
         for (Object obj : sSel.toList()) {
           if (obj instanceof IEvent) {
             IEvent event = ( IEvent )obj;
-            SelectionPartnerMarker.this.selectedEvents.add( event );
+            if (event.getProcess().getTrace() != trace) {
+            	return;
+            }
+            newSelectedEvents.add( event );
             if (event.getPartnerEvent() != null) {
-              SelectionPartnerMarker.this.partnerEvents.add( event.getPartnerEvent() );
+              newPartnerEvents.add( event.getPartnerEvent() );
             }
           }
         }
+        SelectionPartnerMarker.this.selectedEvents = newSelectedEvents;
+        SelectionPartnerMarker.this.partnerEvents = newPartnerEvents;
       }
     });
   }
 
   @Override
   public Color getBackgroundColor( final int type ) {
-    return Display.getDefault().getSystemColor( SWT.COLOR_GRAY );
+    return bgColor;
   }
 
   @Override
   public Color getForegroundColor( final int type ) {
-    return Display.getDefault().getSystemColor( SWT.COLOR_BLACK );
+    return fgColor;
   }
 
   @Override
